24c8e1e646db006b7ca92afacb55354f552f6eb2
gkchic <gkchic@outlook.com>
2015-01-05 13:44:01 +0100

    New content template
    New content template with top navigation

--- gulpfile.js
+++ gulpfile.js
@@ -33,7 +33,7 @@ gulp.task('site',['indexSite'], function() {
         }
     };
     // Compile a template for rendering each page
-    var template = hogan.compile(String(fs.readFileSync('tpl/ink.html')));
+    var template = hogan.compile(String(fs.readFileSync('tpl/ink_org.html')));
 
     return gulp.src('docs/**/*.md')
 
