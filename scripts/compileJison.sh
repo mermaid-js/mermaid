b97e5ff5b0bb474a81415a8f5eaba1919743fd4d
knsv <knut@sveido.com>
2015-07-03 08:52:34 +0200

    Cleanup

--- scripts/compileJison.sh
+++
@@ -1,3 +0,0 @@
-#sed s/this.parseError\(errStr/console.log\(errStr/< src/parser/flow.js > src/parser/flowParser.js
-#cp src/parser/flowParser.js src/parser/flow.js
-#rm src/parser/flowParser.js
---
+++ scripts/genDocs.sh
@@ -0,0 +1,4 @@
+rm -r htmlDocs
+mkdir htmlDocs
+cp -r ./docs/img htmlDocs
+../docker/docker -i ../mermaid/ -c emacs -x "*git*|*travis*|*bin*|*dist*|*node_modules*|*gulp*|*lib*|*editor*|*conf*|*scripts*|*test*|*htmlDocs*" --extras mermaid -w -o htmlDocs
