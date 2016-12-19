71125e7b868adb930b2c0e7b0ef16c71267c5f27
whyzdev <whyzdev>
2016-12-19 00:09:12 -0500

    change " to ' per code climate

--- test/cli_test-samples.js
+++ test/cli_test-samples.js
@@ -1,20 +1,17 @@
 'use strict';
 var exec = require('child_process').exec;
-var fs = require('fs')
-  , path = require('path')
+var path = require('path')
 
   var test = require('tape')
-  , async = require('async')
-  , clone = require('clone')
   , rimraf = require('rimraf')
 
-var test_dir = "test/fixtures/samples/".replace('/',path.sep)
+var test_dir = 'test/fixtures/samples/'.replace('/',path.sep)
 
 rimraf.sync(test_dir+'*.actual.*');
  
 function exec_mermaid(args, verify) {
   exec('bin/mermaid.js ' + args, 
-    {env: {PATH: "./node_modules/.bin"+path.delimiter+process.env.PATH}}, 
+    {env: {PATH: './node_modules/.bin'+path.delimiter+process.env.PATH}}, 
     function(error, stdout, stderr) {
       console.log('error:',error,'\nstdout:\n',stdout,'\nstderr:\n',stderr);
       verify(error, stdout, stderr);
@@ -23,19 +20,17 @@ function exec_mermaid(args, verify) {
 
 test('mermaid cli help', function(t) {
   t.plan(1);
-  var args = [ "--help", ]
-  exec_mermaid(args.join(" "),
-    function(error, stdout, stderr) {
-      t.notOk(error, 'no error')
+  var args = [ '--help' ]
+  exec_mermaid(args.join(' '), function(error, stdout, stderr) {
+      t.notOk(stderr, 'no error')
       t.end()
     });
 });
 
 test('mermaid cli help', function(t) {
   t.plan(1);
-  var args = [ "--badopt", ]
-  exec_mermaid(args.join(" "),
-    function(error, stdout, stderr) {
+  var args = [ '--badopt' ]
+  exec_mermaid(args.join(' '), function(error, stdout, stderr) {
       t.ok(stderr, 'should get error')
       t.end()
     });
@@ -44,13 +39,12 @@ test('mermaid cli help', function(t) {
 //todo
 test.skip('sequence syntax error', function(t) {
   t.plan(1);
-  var args = [ "--svg",
-    "--outputDir=" + test_dir,
-    "--outputSuffix=.actual",
-    test_dir+"sequence_err.mmd",
+  var args = [ '--svg',
+    '--outputDir=' + test_dir,
+    '--outputSuffix=.actual',
+    test_dir+'sequence_err.mmd'
   ]
-  exec_mermaid(args.join(" "),
-    function(error, stdout, stderr) {
+  exec_mermaid(args.join(' '), function(error, stdout, stderr) {
       t.ok(stderr, 'should get error')
       t.end()
     });
@@ -59,14 +53,13 @@ test.skip('sequence syntax error', function(t) {
 ['', 'fo', 'tspan', 'old'].forEach(function(textPlacement) {
   test('sequence svg text placelment: '+textPlacement, function(t) {
     t.plan(1);
-    var args = [ "--svg",
-      "--outputDir=" + test_dir,
-      "--outputSuffix="+(textPlacement ? "_"+textPlacement : "")+".actual",
-      textPlacement ? "--sequenceConfig="+test_dir+"sequence_text_"+textPlacement+".cfg" : "",  
-      test_dir+"sequence_text.mmd",
+    var args = [ '--svg',
+      '--outputDir=' + test_dir,
+      '--outputSuffix='+(textPlacement ? '_'+textPlacement : '')+'.actual',
+      textPlacement ? '--sequenceConfig='+test_dir+'sequence_text_'+textPlacement+'.cfg' : '',  
+      test_dir+'sequence_text.mmd'
     ]
-    exec_mermaid(args.join(" "),
-      function(error, stdout, stderr) {
+    exec_mermaid(args.join(' '), function(error, stdout, stderr) {
         t.notOk(stderr, 'no error')
         t.end()
       });
@@ -75,13 +68,12 @@ test.skip('sequence syntax error', function(t) {
 
 test('sequence png', function(t) {
   t.plan(1);
-  var args = [ "--png",
-    "--outputDir=" + test_dir,
-    "--outputSuffix=.actual",
-    test_dir+"sequence_text.mmd",
+  var args = [ '--png',
+    '--outputDir=' + test_dir,
+    '--outputSuffix=.actual',
+    test_dir+'sequence_text.mmd'
   ]
-  exec_mermaid(args.join(" "),
-    function(error, stdout, stderr) {
+  exec_mermaid(args.join(' '), function(error, stdout, stderr) {
       t.notOk(stderr, 'no error')
       t.end()
     });
@@ -89,13 +81,12 @@ test('sequence png', function(t) {
 
 test('flowchart svg text', function(t) {
   t.plan(1);
-  var args = [ "--svg",
-    "--outputDir=" + test_dir,
-    "--outputSuffix=.actual",
-    test_dir+"flowchart_text.mmd",
+  var args = [ '--svg',
+    '--outputDir=' + test_dir,
+    '--outputSuffix=.actual',
+    test_dir+'flowchart_text.mmd'
   ]
-  exec_mermaid(args.join(" "),
-    function(error, stdout, stderr) {
+  exec_mermaid(args.join(' '), function(error, stdout, stderr) {
       t.notOk(stderr, 'no error')
       t.end()
     });
