ff0ab048df8988ac81fe15015ab2cf05ee29dc2c
Joshua Colvin <joshua@joshuacolvin.net>
2017-01-07 12:57:32 -0500

    Add unit tests for isSubstringInArray utility function

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -202,3 +202,15 @@ describe('when cloning CSS ', function () {
     });
 });
 
+describe('when finding substring in array ', function () {
+    it('should return the array index that contains the substring', function () {
+        var arr = ['stroke:val1', 'fill:val2'];
+        var result = utils.isSubstringInArray('fill', arr);
+        expect(result).toEqual(1);
+    });
+    it('should return -1 if the substring is not found in the array', function () {
+      var arr = ['stroke:val1', 'stroke-width:val2'];
+      var result = utils.isSubstringInArray('fill', arr);
+      expect(result).toEqual(-1);
+    });
+});
