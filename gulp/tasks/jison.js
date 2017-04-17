4032824c79800ef5b584e5347804037f9bd55e93
Tyler Long <tyler4long@gmail.com>
2017-04-17 17:13:37 +0800

    Format code

--- gulp/tasks/jison.js
+++ gulp/tasks/jison.js
@@ -12,12 +12,12 @@ gulp.task('jison', function () {
 
 gulp.task('jison_legacy', function () {
   shell.task([
-    'node node_modules/jison/lib/cli.js src/diagrams/classDiagram/parser/classDiagram.jison       -o src/diagrams/classDiagram/parser/classDiagram.js',
+    'node node_modules/jison/lib/cli.js src/diagrams/classDiagram/parser/classDiagram.jison -o src/diagrams/classDiagram/parser/classDiagram.js',
     'node node_modules/jison/lib/cli.js src/diagrams/sequenceDiagram/parser/sequenceDiagram.jison -o src/diagrams/sequenceDiagram/parser/sequenceDiagram.js',
-    'node node_modules/jison/lib/cli.js src/diagrams/example/parser/example.jison                 -o src/diagrams/example/parser/example.js',
-    'node node_modules/jison/lib/cli.js src/diagrams/flowchart/parser/flow.jison                  -o src/diagrams/flowchart/parser/flow.js',
-    'node node_modules/jison/lib/cli.js src/diagrams/flowchart/parser/dot.jison                  -o src/diagrams/flowchart/parser/dot.js',
-    'node node_modules/jison/lib/cli.js src/diagrams/gitGraph/parser/gitGraph.jison               -o src/diagrams/gitGraph/parser/gitGraph.js',
-    'node node_modules/jison/lib/cli.js src/diagrams/gantt/parser/gantt.jison                     -o src/diagrams/gantt/parser/gantt.js'
+    'node node_modules/jison/lib/cli.js src/diagrams/example/parser/example.jison -o src/diagrams/example/parser/example.js',
+    'node node_modules/jison/lib/cli.js src/diagrams/flowchart/parser/flow.jison -o src/diagrams/flowchart/parser/flow.js',
+    'node node_modules/jison/lib/cli.js src/diagrams/flowchart/parser/dot.jison -o src/diagrams/flowchart/parser/dot.js',
+    'node node_modules/jison/lib/cli.js src/diagrams/gitGraph/parser/gitGraph.jison -o src/diagrams/gitGraph/parser/gitGraph.js',
+    'node node_modules/jison/lib/cli.js src/diagrams/gantt/parser/gantt.jison -o src/diagrams/gantt/parser/gantt.js'
   ])
 })
