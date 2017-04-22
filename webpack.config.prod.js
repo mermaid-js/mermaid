73027d5cec0cb297daf0f713383fb4ffdd6516ef
Tyler Long <tyler4long@gmail.com>
2017-04-22 16:04:16 +0800

    yarn dist generates minified css files

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,5 @@
-import { webConfig, nodeConfig } from './webpack.config.base.js'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+import { webConfig, nodeConfig, lessConfig } from './webpack.config.base.js'
 
 const config1 = webConfig()
 config1.output.filename = '[name].min.js'
@@ -10,4 +11,8 @@ config2.output.filename = '[name].slim.min.js'
 const config3 = nodeConfig()
 config3.output.filename = '[name].min.js'
 
-export default [config1, config2, config3]
+const config4 = lessConfig()
+config4.output.filename = '[name].min.css'
+config4.plugins = [ new ExtractTextPlugin('[name].min.css') ]
+
+export default [config1, config2, config3, config4]
