ad72cfac461908f53b26837283b25c0c644f4903
Tyler Long <tyler4long@gmail.com>
2017-06-01 10:48:33 +0800

    Format gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,114 +1,108 @@
-var gulp = require('gulp');
-var data = require('gulp-data');
-var fm = require('front-matter');
-var marked = require('marked');
-var fs = require('fs');
-var es = require('event-stream');
-var hogan = require('hogan.js');
-var ext_replace = require('gulp-ext-replace');
+var gulp = require('gulp')
+var data = require('gulp-data')
+var fm = require('front-matter')
+var marked = require('marked')
+var fs = require('fs')
+var es = require('event-stream')
+var hogan = require('hogan.js')
+var extReplace = require('gulp-ext-replace')
 
+var fileList = []
+gulp.task('indexSite', function () {
+  return gulp.src('docs/**/*.md')
+    // Extract YAML front-matter, convert content to markdown via gulp-data
+    .pipe(data(function (file) {
+      var content = fm(String(file.contents))
+      var name = file.relative.substr(0, file.relative.length - 3)
+      fileList.push({ name: name, title: content.attributes.title, order: content.attributes.order })
+    }))
+})
 
-var fileList = [];
-gulp.task('indexSite', function() {
-    return gulp.src('docs/**/*.md')
-        // Extract YAML front-matter, convert content to markdown via gulp-data
-        .pipe(data(function(file) {
-            var content = fm(String(file.contents));
-            var name = file.relative.substr(0,file.relative.length-3);
-            fileList.push({name:name, title:content.attributes.title, order:content.attributes.order});
-        }));
-});
+function compare (a, b) {
+  if (typeof a.order === 'undefined') {
+    return 1
+  }
+  if (typeof b.order === 'undefined') {
+    return -1
+  }
+  if (a.order < b.order) { return -1 }
+  if (a.order > b.order) { return 1 }
 
-function compare(a,b){
-    if(typeof a.order === 'undefined'){
-        return 1;
-    }
-    if(typeof b.order === 'undefined'){
-        return -1;
-    }
-    if (a.order < b.order)
-        return -1;
-    if (a.order > b.order)
-        return 1;
-
-    return 0;
+  return 0
 }
 
-function sortNav(nav){
-    return nav.sort(compare);
+function sortNav (nav) {
+  return nav.sort(compare)
 }
 
-gulp.task('site',['indexSite'], function() {
-    var renderer = new marked.Renderer();
-    renderer.table = function (header, body) {
-        return "<table class=\"ink-table bordered hover alternating\">" +header + body+ "</table>";
-    };
-    renderer.code = function (code, language) {
-        if(code.match(/^sequenceDiagram/)||code.match(/^graph/)||code.match(/^info/)||code.match(/^gantt/)){
+gulp.task('site', ['indexSite'], function () {
+  var renderer = new marked.Renderer()
+  renderer.table = function (header, body) {
+    return '<table class="ink-table bordered hover alternating">' + header + body + '</table>'
+  }
+  renderer.code = function (code, language) {
+    if (code.match(/^sequenceDiagram/) || code.match(/^graph/) || code.match(/^info/) || code.match(/^gantt/)) {
+      return '<div class="mermaid">' + code + '</div>'
+    } else {
+      return '<pre><code>' + code + '</code></pre>'
+    }
+  }
+  // Compile a template for rendering each page
+  var template = hogan.compile(String(fs.readFileSync('tpl/content.html')))
 
-            return '<div class="mermaid">'+code+'</div>';
+  return gulp.src('docs/**/*.md')
+
+    // Extract YAML front-matter, convert content to markdown via gulp-data
+    .pipe(data(function (file) {
+      var content = fm(String(file.contents))
+      // console.log('yaml:',file.relative);
+      file.contents = Buffer.from(marked(content.body, { renderer: renderer }))
+      file.title = content.attributes.title
+      // console.log(content.attributes);
+      console.log(content.attributes.title)
+      file.bapa = 'chimp'
+
+      var name = file.relative.substr(0, file.relative.length - 3)
+      file.name = name
+
+      var list = []
+
+      var sortedList = sortNav(fileList)
+
+      console.log(sortedList)
+
+      sortedList.forEach(function (item) {
+        var active = false
+        if (item.name === name) {
+          active = true
         }
-        else{
-            return '<pre><code>'+code+'</code></pre>';
-        }
-    };
-    // Compile a template for rendering each page
-    var template = hogan.compile(String(fs.readFileSync('tpl/content.html')));
+        list.push({ name: item.name, title: item.title, active: active })
+      })
+      file.list = list
+      // console.log(list);
+      return content.attributes
+    }))
 
-    return gulp.src('docs/**/*.md')
+    // Run each file through a template
+    .pipe(es.map(function (file, cb) {
+      // console.log('file:',fileList);
+      file.contents = Buffer.from(template.render(file))
 
-        // Extract YAML front-matter, convert content to markdown via gulp-data
-        .pipe(data(function(file) {
-            var content = fm(String(file.contents));
-            //console.log('yaml:',file.relative);
-            file.contents = new Buffer(marked(content.body, {renderer:renderer}));
-            file.title = content.attributes.title;
-            //console.log(content.attributes);
-            console.log(content.attributes.title);
-            file.bapa = 'chimp';
+      cb(null, file)
+    }))
+    .pipe(extReplace('.html'))
+    // Output to build directory
+    .pipe(gulp.dest('./'))
+})
 
-            var name = file.relative.substr(0,file.relative.length-3);
-            file.name = name;
+gulp.task('www', ['site'], function () {
+  console.log('Starting webserver. Running at: http://127.0.0.1:3000/')
+  console.log('Hold ctrl+c to quit.')
+  var express = require('express')
+  var app = express()
 
-            var list = [];
+  app.use('/dist/', express.static('/Users/knut/source/mermaid/dist'))
+  app.use('/', express.static('./'))
 
-            var sortedList = sortNav(fileList);
-
-            console.log(sortedList);
-
-            sortedList.forEach(function(item){
-                var active = false;
-                if(item.name === name){
-                    active = true;
-                }
-                list.push({name:item.name,title:item.title,active:active});
-            });
-            file.list = list;
-            //console.log(list);
-            return content.attributes;
-        }))
-
-
-        // Run each file through a template
-        .pipe(es.map(function(file, cb) {
-            //console.log('file:',fileList);
-            file.contents = new Buffer(template.render(file));
-
-            cb(null, file);
-        }))
-        .pipe(ext_replace('.html'))
-        // Output to build directory
-        .pipe(gulp.dest('./'));
-});
-
-gulp.task('www', ['site'], function() {
-    console.log('Starting webserver. Running at: http://127.0.0.1:3000/');
-    console.log('Hold ctrl+c to quit.');
-    var express = require('express');
-    var app = express();
-
-    app.use('/dist/', express.static('/Users/knut/source/mermaid/dist'));
-    app.use('/', express.static('./'));
-
-    app.listen(process.env.PORT || 3000);
-});
+  app.listen(process.env.PORT || 3000)
+})
