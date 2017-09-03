81132bf0edaa79b77598c7df96d8c3e3009dd884
Tyler Long <tyler4long@gmail.com>
2017-09-03 12:16:32 +0800

    Remove jison.sh

--- scripts/jison.sh
+++
@@ -1,7 +0,0 @@
-node node_modules/jison/lib/cli.js src/diagrams/classDiagram/parser/classDiagram.jison       -o src/diagrams/classDiagram/parser/classDiagram.js
-node node_modules/jison/lib/cli.js src/diagrams/sequenceDiagram/parser/sequenceDiagram.jison -o src/diagrams/sequenceDiagram/parser/sequenceDiagram.js
-node node_modules/jison/lib/cli.js src/diagrams/example/parser/example.jison                 -o src/diagrams/example/parser/example.js
-node node_modules/jison/lib/cli.js src/diagrams/flowchart/parser/flow.jison                  -o src/diagrams/flowchart/parser/flow.js
-node node_modules/jison/lib/cli.js src/diagrams/flowchart/parser/dot.jison                   -o src/diagrams/flowchart/parser/dot.js
-node node_modules/jison/lib/cli.js src/diagrams/gantt/parser/gantt.jison                     -o src/diagrams/gantt/parser/gantt.js
-node node_modules/jison/lib/cli.js src/diagrams/gitGraph/parser/gitGraph.jison               -o src/diagrams/gitGraph/parser/gitGraph.js
