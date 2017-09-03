64b5880444412b036ebd071245d3c35f7b5636c2
Tyler Long <tyler4long@gmail.com>
2017-09-03 12:19:34 +0800

    Move gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,13 @@
 var requireDir = require('require-dir')
+var gulp = require('gulp')
+var jison = require('gulp-jison')
+var filelog = require('gulp-filelog')
+
+gulp.task('jison', function () {
+  return gulp.src('./src/**/*.jison')
+    .pipe(filelog('Jison file:'))
+    .pipe(jison({ 'token-stack': true }))
+    .pipe(gulp.dest('./src/'))
+})
 
 requireDir('./gulp/tasks')
