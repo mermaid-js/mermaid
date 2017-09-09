57db10a4c0ae1f93c34caefb72d0460123fec288
Tyler Long <tyler4long@gmail.com>
2017-09-09 21:51:12 +0800

    Make yarn release work

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,18 +1,11 @@
 import ExtractTextPlugin from 'extract-text-webpack-plugin'
-import { webConfig, nodeConfig, lessConfig } from './webpack.config.base.js'
+import { jsConfig, lessConfig } from './webpack.config.base.js'
 
-const minConfig = webConfig()
+const minConfig = jsConfig()
 minConfig.output.filename = '[name].min.js'
-
-const slimMinConfig = webConfig()
-slimMinConfig.externals = ['fs', 'd3']
-slimMinConfig.output.filename = '[name].slim.min.js'
-
-const apiMinConfig = nodeConfig()
-apiMinConfig.output.filename = '[name].min.js'
 
 const cssMinConfig = lessConfig()
 cssMinConfig.output.filename = '[name].min.css'
 cssMinConfig.plugins = [ new ExtractTextPlugin('[name].min.css') ]
 
-export default [minConfig, slimMinConfig, apiMinConfig, cssMinConfig]
+export default [minConfig, cssMinConfig]
