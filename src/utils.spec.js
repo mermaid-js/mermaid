794a00866ef1db2db8b17af0e35597274f7aae49
Alex Hart <alex@hartcode.com>
2019-01-19 08:49:12 -0600

    Improved test coverage for utils.js

--- src/utils.spec.js
+++ src/utils.spec.js
@@ -23,17 +23,53 @@ describe('when detecting chart type ', function () {
     const type = utils.detectType(str)
     expect(type).toBe('git')
   })
+  it('should handle a sequenceDiagram defintion for gitGraph', function () {
+    const str = 'sequenceDiagram TB:\nbfs1:queue'
+    const type = utils.detectType(str)
+    expect(type).toBe('sequence')
+  })
+  it('should handle a classDiagram defintion for gitGraph', function () {
+    const str = 'classDiagram TB:\nbfs1:queue'
+    const type = utils.detectType(str)
+    expect(type).toBe('class')
+  })
+  it('should handle a gantt defintion for gitGraph', function () {
+    const str = 'gantt TB:\nbfs1:queue'
+    const type = utils.detectType(str)
+    expect(type).toBe('gantt')
+  })
 })
 
 describe('when finding substring in array ', function () {
   it('should return the array index that contains the substring', function () {
-    const arr = ['stroke:val1', 'fill:val2']
+    const arr = [ 'stroke:val1', 'fill:val2' ]
     const result = utils.isSubstringInArray('fill', arr)
     expect(result).toEqual(1)
   })
   it('should return -1 if the substring is not found in the array', function () {
-    const arr = ['stroke:val1', 'stroke-width:val2']
+    const arr = [ 'stroke:val1', 'stroke-width:val2' ]
     const result = utils.isSubstringInArray('fill', arr)
     expect(result).toEqual(-1)
+  })
+})
+
+describe('when converting an interpolate to a curve', function () {
+  it('should return the defualt curve because the interpolate is falsy', function () {
+    const interpolate = ''
+    const defaultCurve = ''
+    const result = utils.interpolateToCurve(interpolate, defaultCurve)
+    expect(result).toEqual(defaultCurve)
+  })
+  it('should return the d3 curve name, because the interpolate is found in d3', function () {
+    const interpolate = 'basis'
+    const defaultCurve = undefined
+    const result = utils.interpolateToCurve(interpolate, defaultCurve)
+    expect(result).toEqual('basis')
+  })
+  it('should return the default curve name because the interpolate is not in d3[]', function () {
+    const interpolate = 'curveUnknown'
+    const defaultCurve = 'curveBasis'
+    const result = utils.interpolateToCurve(interpolate, defaultCurve)
+    expect(result).toEqual(defaultCurve)
   })
 })
