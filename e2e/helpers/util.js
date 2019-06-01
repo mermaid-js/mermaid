86fcbf9c5e92ddfd984e250e67808c111a9c972e
Knut Sveidqvist <knut.sveidqvist@ipiccolo.com>
2019-06-01 19:21:15 +0200

    Added e2e tests and snapshot files

--- e2e/helpers/util.js
+++ e2e/helpers/util.js
@@ -1,6 +1,7 @@
+/* eslint-env jest */
 import { Base64 } from 'js-base64'
 
-const mermaidUrl = (graphStr, options) => {
+export const mermaidUrl = (graphStr, options) => {
   const obj = {
     code: graphStr,
     mermaid: options
@@ -10,4 +11,16 @@ const mermaidUrl = (graphStr, options) => {
   return 'http://localhost:9000/e2e.html?graph=' + Base64.encodeURI(objStr)
 }
 
-export default mermaidUrl
+export const imgSnapshotTest = async (page, graphStr, options) => {
+  return new Promise(async resolve => {
+    const url = mermaidUrl(graphStr, options)
+
+    await page.goto(url)
+
+    const image = await page.screenshot()
+
+    expect(image).toMatchImageSnapshot()
+    resolve()
+  })
+  // page.close()
+}
