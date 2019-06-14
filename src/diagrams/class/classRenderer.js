f057bd1e89a10dc3fe373e671fd69561d2afc928
Dan Shai <danshai2013@gmail.com>
2019-06-14 08:01:51 +0300

    fixed class diagram 2nd pr

--- src/diagrams/class/classRenderer.js
+++ src/diagrams/class/classRenderer.js
@@ -1,9 +1,9 @@
-import * as d3 from 'd3';
-import dagre from 'dagre-layout';
-import graphlib from 'graphlibrary';
-import { logger } from '../../logger';
-import classDb from './classDb';
-import { parser } from './parser/classDiagram';
+import * as d3 from 'd3'
+import dagre from 'dagre-layout'
+import graphlib from 'graphlibrary'
+import { logger } from '../../logger'
+import classDb from './classDb'
+import { parser } from './parser/classDiagram'
 
 parser.yy = classDb
 
