9753578903e7588c42223e557cd44c5f83b720bb
Knut Sveidqvist <knut@sveido.com>
2019-07-14 02:44:46 -0700

    Removed filelog as it causes a security warning

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,20 @@
 import gulp from 'gulp'
 import jison from 'gulp-jison'
-import filelog from 'gulp-filelog'
+import print from 'gulp-print'
+// const es = require('event-stream')
+
+const logFile = function (es) {
+  return es.map(function (file, cb) {
+    log(file.path)
+    return cb()
+  })
+}
 
 gulp.task('jison', function () {
   return gulp.src('./src/**/*.jison')
-    .pipe(filelog('Jison file:'))
+    // .pipe(filelog('Jison file:'))
+    // .pipe(logFile(es))
+    .pipe(print())
     .pipe(jison({ 'token-stack': true }))
     .pipe(gulp.dest('./src/'))
 })
