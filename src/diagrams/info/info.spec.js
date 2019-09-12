e67b8c86d6d7fed8a9b0649807f70577fddd5b31
knsv <knsv@sveido.com>
2019-09-12 12:55:31 -0700

    #931 Aligning code standard

--- src/diagrams/info/info.spec.js
+++ src/diagrams/info/info.spec.js
@@ -1,15 +1,15 @@
 /* eslint-env jasmine */
-describe('when parsing an info graph it', function () {
-  var ex
-  beforeEach(function () {
-    ex = require('./parser/info').parser
-    ex.yy = require('./infoDb')
-  })
+describe('when parsing an info graph it', function() {
+  var ex;
+  beforeEach(function() {
+    ex = require('./parser/info').parser;
+    ex.yy = require('./infoDb');
+  });
 
-  it('should handle an info definition', function () {
+  it('should handle an info definition', function() {
     var str = `info
-    showInfo`
+    showInfo`;
 
-    ex.parse(str)
-  })
-})
+    ex.parse(str);
+  });
+});
