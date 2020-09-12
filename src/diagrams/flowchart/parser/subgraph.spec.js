a14ff7820efb6907bf9281bdd05c8387b7fa02e2
Knut Sveidqvist <knut.sveidqvist@ipiccolo.com>
2020-09-12 13:40:25 +0200

    #1676 Fix for tests

--- src/diagrams/flowchart/parser/subgraph.spec.js
+++ src/diagrams/flowchart/parser/subgraph.spec.js
@@ -85,7 +85,7 @@ describe('when parsing subgraphs', function() {
     const subgraph = subgraphs[0];
     expect(subgraph.nodes.length).toBe(1);
     expect(subgraph.nodes[0]).toBe('A');
-    expect(subgraph.id).toBe('1test');
+    expect(subgraph.id).toBe('flowchart-1test');
   });
 
   it('should handle subgraphs1', function() {
