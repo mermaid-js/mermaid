acc2392c3d8e40c36557b04abdf3b5e3f4c50484
Neil Cuzon <58763315+NeilCuzon@users.noreply.github.com>
2020-11-15 05:00:54 -0800

    Update mermaidAPI.js

--- src/mermaidAPI.js
+++ src/mermaidAPI.js
@@ -1,6 +1,6 @@
 /**
  * 
- * **Edit this Page** [![N|Solid](img/GitHub-Mark-32px.png)](https://github.com/mermaid-js/mermaid/blob/develop/src/mermaidAPI.js) 
+ *Edit this Page[![N|Solid](img/GitHub-Mark-32px.png)](https://github.com/mermaid-js/mermaid/blob/develop/src/mermaidAPI.js) 
  *
  *This is the api to be used when optionally handling the integration with the web page, instead of using the default integration provided by mermaid.js.
  *
