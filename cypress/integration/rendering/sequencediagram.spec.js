06f74a8c153c4805e96e54726cc99fe06b9a58cb
Marc Faber <faber@genodata.de>
2020-11-17 14:34:48 +0100

    updated integration tests

--- cypress/integration/rendering/sequencediagram.spec.js
+++ cypress/integration/rendering/sequencediagram.spec.js
@@ -564,7 +564,7 @@ context('Sequence diagram', () => {
           expect(svg).to.have.attr('width', '100%');
           expect(svg).to.have.attr('height');
           const height = parseFloat(svg.attr('height'));
-          expect(height).to.eq(920);
+          expect(height).to.eq(960);
           const style = svg.attr('style');
           expect(style).to.match(/^max-width: [\d.]+px;$/);
           const maxWidthValue = parseFloat(style.match(/[\d.]+/g).join(''));
@@ -604,7 +604,7 @@ context('Sequence diagram', () => {
         .should((svg) => {
           const height = parseFloat(svg.attr('height'));
           const width = parseFloat(svg.attr('width'));
-          expect(height).to.eq(920);
+          expect(height).to.eq(960);
           // use within because the absolute value can be slightly different depending on the environment ±5%
           expect(width).to.be.within(820 * .95, 820 * 1.05);
           expect(svg).to.not.have.attr('style');
--- cypress/integration/rendering/stateDiagram-v2.spec.js
+++ cypress/integration/rendering/stateDiagram-v2.spec.js
@@ -369,7 +369,7 @@ describe('State diagram', () => {
         expect(svg).to.have.attr('width', '100%');
         expect(svg).to.have.attr('height');
         const height = parseFloat(svg.attr('height'));
-        expect(height).to.eq(177);
+        expect(height).to.eq(178);
         const style = svg.attr('style');
         expect(style).to.match(/^max-width: [\d.]+px;$/);
         const maxWidthValue = parseFloat(style.match(/[\d.]+/g).join(''));
@@ -391,7 +391,7 @@ describe('State diagram', () => {
       .should((svg) => {
         const height = parseFloat(svg.attr('height'));
         const width = parseFloat(svg.attr('width'));
-        expect(height).to.eq(177);
+        expect(height).to.eq(178);
         // use within because the absolute value can be slightly different depending on the environment ±5%
         expect(width).to.be.within(135 * .95, 135 * 1.05);
         expect(svg).to.not.have.attr('style');
