8db827e429e4fda75b40616c465bbabf2d697b3c
Xavier Stouder <xavier@stouder.io>
2021-08-26 11:37:14 +0200

    chore: fix regexp

--- src/diagrams/class/svgDraw.js
+++ src/diagrams/class/svgDraw.js
@@ -274,7 +274,7 @@ export const drawClass = function (elem, classDef, conf) {
 };
 
 export const parseMember = function (text) {
-  const fieldRegEx = /^(\+|-|~|#)?(\w+)(~\w+~|\[\])?\s+(\w+) *(\$)?$/;
+  const fieldRegEx = /^(\+|-|~|#)?(\w+)(~\w+~|\[\])?\s+(\w+) *(\*|\$)?$/;
   const methodRegEx = /^([+|\-|~|#])?(\w+) *\( *(.*)\) *(\*|\$)? *(\w*[~|[\]]*\s*\w*~?)$/;
 
   let fieldMatch = text.match(fieldRegEx);
