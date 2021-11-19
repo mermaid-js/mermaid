7ce383078d2e2ccb6d635e23a3da694ba5ed3d54
Pablo Bendersky <pbendersky@quadiontech.com>
2021-11-19 13:40:39 -0300

    Fixed state nodes with spaces being rendered as rectWithTitle instead of rect

--- src/diagrams/state/stateRenderer-v2.js
+++ src/diagrams/state/stateRenderer-v2.js
@@ -60,25 +60,9 @@ const setupNode = (g, parent, node, altFlag) => {
 
     // Build of the array of description strings accordinging
     if (node.description) {
-      if (Array.isArray(nodeDb[node.id].description)) {
-        // There already is an array of strings,add to it
-        nodeDb[node.id].shape = 'rectWithTitle';
-        nodeDb[node.id].description.push(node.description);
-      } else {
-        if (nodeDb[node.id].description.length > 0) {
-          // if there is a description already transformit to an array
-          nodeDb[node.id].shape = 'rectWithTitle';
-          if (nodeDb[node.id].description === node.id) {
-            // If the previous description was the is, remove it
-            nodeDb[node.id].description = [node.description];
-          } else {
-            nodeDb[node.id].description = [nodeDb[node.id].description, node.description];
-          }
-        } else {
-          nodeDb[node.id].shape = 'rect';
-          nodeDb[node.id].description = node.description;
-        }
-      }
+      nodeDb[node.id].shape = 'rect';
+      nodeDb[node.id].description = node.description;
+
       nodeDb[node.id].description = common.sanitizeTextOrArray(
         nodeDb[node.id].description,
         getConfig()
