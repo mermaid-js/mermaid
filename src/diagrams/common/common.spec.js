c37ff47ee39cb48c1d43abc7a6ca697a6bdf5409
Sidharth Vinod <sidharthv96@gmail.com>
2022-08-24 12:33:32 +0530

    Add nested test

--- src/diagrams/common/common.spec.js
+++ src/diagrams/common/common.spec.js
@@ -106,7 +106,7 @@ describe('Sanitize text', function () {
 
 describe('generic parser', function () {
   it('should parse generic types', function () {
-    const result = parseGenericTypes('test~T~');
-    expect(result).toEqual('test<T>');
+    expect(parseGenericTypes('test~T~')).toEqual('test<T>');
+    expect(parseGenericTypes('test~Array~Array~string~~~')).toEqual('test<Array<Array<string>>>');
   });
 });
