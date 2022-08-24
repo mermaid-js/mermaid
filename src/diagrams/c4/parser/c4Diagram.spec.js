aee1a87347da0b04dd555000cccb76a4730002f5
Daniel Bartholomae <daniel@bartholomae.name>
2022-08-24 23:22:50 +0200

    Move test for basic Person to separate file

--- src/diagrams/c4/parser/c4Diagram.spec.js
+++ src/diagrams/c4/parser/c4Diagram.spec.js
@@ -12,35 +12,6 @@ describe('parsing a C4 diagram', function () {
     c4.parser.yy.clear();
   });
 
-  it('should parse a C4 diagram with one Person correctly', function () {
-    c4.parser.parse(`C4Context
-title System Context diagram for Internet Banking System
-Person(customerA, "Banking Customer A", "A customer of the bank, with personal bank accounts.")`);
-
-    const yy = c4.parser.yy;
-    expect(yy.getC4Type()).toBe('C4Context');
-    expect(yy.getTitle()).toBe('System Context diagram for Internet Banking System');
-
-    const shapes = yy.getC4ShapeArray();
-    expect(shapes.length).toBe(1);
-    const onlyShape = shapes[0];
-
-    expect(onlyShape).toEqual({
-      alias: 'customerA',
-      descr: {
-        text: 'A customer of the bank, with personal bank accounts.',
-      },
-      label: {
-        text: 'Banking Customer A',
-      },
-      parentBoundary: 'global',
-      typeC4Shape: {
-        text: 'person',
-      },
-      wrap: false,
-    });
-  });
-
   it('should handle a trailing whitespaces after statements', function () {
     const whitespace = ' ';
     const rendered = c4.parser.parse(`C4Context${whitespace}
