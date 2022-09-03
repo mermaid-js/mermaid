cb4935258ce9653969cf55c02fef03abc61521e9
Alois Klink <alois@aloisklink.com>
2022-09-03 23:34:19 +0100

    style: forbid using `console` in mermaid src code
    Adds an eslint rule forbidding using `console` in the mermaid source
    code. Instead, the `src/logger` should be used instead, so that
    websites can disable logging.
    
    This is allowed in the `cypress/` and `demos/` folder.
    
    I've also removed the two instances on `console.log`/`console.error`
    that currently exist in the mermaid source code.

--- src/diagrams/info/infoRenderer.js
+++ src/diagrams/info/infoRenderer.js
@@ -49,7 +49,6 @@ export const draw = (text, id, version, diagObj) => {
     svg.attr('width', 400);
     // svg.attr('viewBox', '0 0 300 150');
   } catch (e) {
-    console.error(e);
     log.error('Error while rendering info diagram');
     log.error(e.message);
   }
