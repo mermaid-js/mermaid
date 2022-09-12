e740325d84deda904218f0f67b02382de7fc3e9f
Sidharth Vinod <sidharthv96@gmail.com>
2022-09-12 11:41:26 +0530

    Unify webpack build

--- .webpack/webpack.config.babel.js
+++ .webpack/webpack.config.babel.js
@@ -3,43 +3,37 @@ import nodeExternals from 'webpack-node-externals';
 import baseConfig from './webpack.config.base';
 
 export default (_env, args) => {
-  switch (args.mode) {
-    case 'development':
-      return [
-        baseConfig,
-        merge(baseConfig, {
-          externals: [nodeExternals()],
-          output: {
-            filename: '[name].core.js',
-          },
-        }),
-      ];
-    case 'production':
-      return [
-        // umd
-        merge(baseConfig, {
-          output: {
-            filename: '[name].min.js',
-          },
-        }),
-        // esm
-        mergeWithCustomize({
-          customizeObject: customizeObject({
-            'output.library': 'replace',
-          }),
-        })(baseConfig, {
-          experiments: {
-            outputModule: true,
-          },
-          output: {
-            library: {
-              type: 'module',
-            },
-            filename: '[name].esm.min.mjs',
-          },
-        }),
-      ];
-    default:
-      throw new Error('No matching configuration was found!');
-  }
+  return [
+    // non-minified
+    baseConfig,
+    // core [To be used by webpack/esbuild/vite etc to bundle mermaid]
+    merge(baseConfig, {
+      externals: [nodeExternals()],
+      output: {
+        filename: '[name].core.js',
+      },
+    }),
+    // umd
+    merge(baseConfig, {
+      output: {
+        filename: '[name].min.js',
+      },
+    }),
+    // esm
+    mergeWithCustomize({
+      customizeObject: customizeObject({
+        'output.library': 'replace',
+      }),
+    })(baseConfig, {
+      experiments: {
+        outputModule: true,
+      },
+      output: {
+        library: {
+          type: 'module',
+        },
+        filename: '[name].esm.min.mjs',
+      },
+    }),
+  ];
 };
