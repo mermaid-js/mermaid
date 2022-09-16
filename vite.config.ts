be7ab54a09a946f316d0c316c2a7884e59bc1a20
Sidharth Vinod <sidharthv96@gmail.com>
2022-09-16 18:42:15 +0530

    Fix lint

--- vite.config.ts
+++ vite.config.ts
@@ -10,9 +10,10 @@ export function jisonPlugin() {
 
     transform(src: string, id: string) {
       if (fileRegex.test(id)) {
+        // eslint-disable-next-line no-console
         console.log('Transforming', id);
         return {
-          // @ts-ignore
+          // @ts-ignore no typings for jison
           code: new Generator(src, { 'token-stack': true }).generate(),
           map: null, // provide source map if available
         };
