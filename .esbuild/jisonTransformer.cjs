c21c026749890990482f5f80993a86a3d1c5eef6
Sidharth Vinod <sidharthv96@gmail.com>
2022-09-20 23:10:05 +0530

    fix(test): No esm exports

--- .esbuild/jisonTransformer.cjs
+++ .esbuild/jisonTransformer.cjs
@@ -1,14 +1,16 @@
 const { Generator } = require('jison');
-exports.transformJison = (src) => {
+exports.transformJison = (src, esm = true) => {
   const parser = new Generator(src, {
     moduleType: 'js',
     'token-stack': true,
   });
   const source = parser.generate({ moduleMain: '() => {}' });
-  const exporter = `
+  const exporter = esm
+    ? `
 	parser.parser = parser;
 	export { parser };
 	export default parser;
-	`;
+	`
+    : '';
   return `${source} ${exporter}`;
 };
