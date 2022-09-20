5791fc9a4d7eaba3e5c3e3e03953fd5c3e460403
Sidharth Vinod <sidharthv96@gmail.com>
2022-09-21 00:15:52 +0530

    Cleanup

--- src/jison/transformer.cjs
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const { transformJison } = require('../../.esbuild/jisonTransformer.cjs');
-
-module.exports = {
-  process(sourceText, sourcePath, options) {
-    return { code: transformJison(sourceText) };
-  },
-};
