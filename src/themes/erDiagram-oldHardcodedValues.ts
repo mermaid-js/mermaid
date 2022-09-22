826cd67cc819b2f55871bf661cb6573f11b532e0
Ashley Engelund (weedySeaDragon @ github) <ashley.engelund@gmail.com>
2022-09-22 11:50:07 -0700

    ER styles: define (old) hardcoded values for themes to use

---
+++ src/themes/erDiagram-oldHardcodedValues.ts
@@ -0,0 +1,8 @@
+/**
+ * @file Values that have been hardcoded in src/diagrams/er/styles.js. These can be used by
+ *   theme-_._ files to maintain display styles until themes, styles, renderers are revised. --
+ *   2022-09-22
+ */
+export const oldAttributeBackgroundColorOdd = '#ffffff';
+export const oldAttributeBackgroundColorEven = '#f2f2f2';
+export const oldAttributeBoxTextColor = '#ff0000';
