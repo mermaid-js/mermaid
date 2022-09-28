bed9b1bb9905428582ebf89ca8e926ee742a0ebe
Knut Sveidqvist <knsv@sveido.com>
2022-09-28 17:17:32 +0200

    Removed test folder

--- packages/rm-mermaid-mindmap-detector/src/mindmapDetector.ts
+++
@@ -1,8 +0,0 @@
-// import type { DiagramDetector } from '../../diagram-api/detectType';
-
-// export const mindmapDetector: DiagramDetector = (txt) => {
-//   return txt.match(/^\s*mindmap/) !== null;
-// };
-export const mindmapDetector = (txt: string) => {
-  return txt.match(/^\s*mindmap/) !== null;
-};
