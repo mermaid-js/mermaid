1e417833f4987b0748d92772a77a18355caf9f0c
Sidharth Vinod <sidharthv96@gmail.com>
2022-11-04 11:29:24 +0530

    Basic webpack

---
+++ tests/webpack/src/index.js
@@ -0,0 +1,6 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-console */
+// const mermaid = require('mermaid');
+import mermaid from 'mermaid';
+
+console.log(mermaid);
---
+++ tests/webpack/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
