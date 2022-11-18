616c969a0397a5289e1f64be63b54120a5aafe4a
Sidharth Vinod <sidharthv96@gmail.com>
2022-11-18 16:11:03 +0530

    chore(split):  Flow type

--- packages/mermaid/src/diagrams/flowchart/flowDetector.ts
+++ packages/mermaid/src/diagrams/flowchart/flowDetector.ts
@@ -1,10 +1,9 @@
-import type { DiagramDetector } from '../../diagram-api/types';
-import type { ExternalDiagramDefinition } from '../../diagram-api/types';
+import type { DiagramDetector, ExternalDiagramDefinition } from '../../diagram-api/types';
 
 const id = 'flowchart';
 
 const detector: DiagramDetector = (txt, config) => {
-  // If we have confired to only use new flow charts this function shohuld always return false
+  // If we have confirmed to only use new flow charts this function should always return false
   // as in not signalling true for a legacy flowchart
   if (config?.flowchart?.defaultRenderer === 'dagre-wrapper') return false;
   return txt.match(/^\s*graph/) !== null;
