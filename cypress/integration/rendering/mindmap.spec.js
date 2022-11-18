b9dcdb00a55a1eeebba430613982129c28940573
Alois Klink <alois@aloisklink.com>
2022-11-18 17:44:59 +0000

    test(e2e): move mindmap tests to mindmap.spec.js
    Currently, we have mindmap tests in the
    cypress/integration/rendering/mermaid.spec.js which is a bit
    odd. They should probably all be in the mindmap.spec.js file.

--- cypress/integration/rendering/mindmap.spec.js
+++ cypress/integration/rendering/mindmap.spec.js
@@ -110,6 +110,76 @@ root
       {}
     );
   });
-
+  it('square shape', () => {
+    imgSnapshotTest(
+      `
+mindmap
+    root[
+      The root
+    ]
+      `,
+      {}
+    );
+    cy.get('svg');
+  });
+  it('rounded rect shape', () => {
+    imgSnapshotTest(
+      `
+mindmap
+    root((
+      The root
+    ))
+      `,
+      {}
+    );
+    cy.get('svg');
+  });
+  it('circle shape', () => {
+    imgSnapshotTest(
+      `
+mindmap
+    root(
+      The root
+    )
+      `,
+      {}
+    );
+    cy.get('svg');
+  });
+  it('default shape', () => {
+    imgSnapshotTest(
+      `
+mindmap
+  The root
+      `,
+      {}
+    );
+    cy.get('svg');
+  });
+  it('adding children', () => {
+    imgSnapshotTest(
+      `
+mindmap
+  The root
+    child1
+    child2
+      `,
+      {}
+    );
+    cy.get('svg');
+  });
+  it('adding grand children', () => {
+    imgSnapshotTest(
+      `
+mindmap
+  The root
+    child1
+      child2
+      child3
+      `,
+      {}
+    );
+    cy.get('svg');
+  });
   /* The end */
 });
