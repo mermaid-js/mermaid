e0cd76e6fd74491414319cd338756c8c8e9661d0
Sidharth Vinod <sidharthv96@gmail.com>
2022-11-18 16:10:25 +0530

    chore(split):  Pie

--- packages/mermaid/src/diagrams/pie/pieDetector.ts
+++ packages/mermaid/src/diagrams/pie/pieDetector.ts
@@ -1,5 +1,20 @@
-import type { DiagramDetector } from '../../diagram-api/types';
+import type { DiagramDetector, ExternalDiagramDefinition } from '../../diagram-api/types';
 
-export const pieDetector: DiagramDetector = (txt) => {
+const id = 'pie';
+
+const detector: DiagramDetector = (txt) => {
   return txt.match(/^\s*pie/) !== null;
 };
+
+const loader = async () => {
+  const { diagram } = await import('./pieDiagram');
+  return { id, diagram };
+};
+
+const plugin: ExternalDiagramDefinition = {
+  id,
+  detector,
+  loader,
+};
+
+export default plugin;
