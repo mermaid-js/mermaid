53a4b4c3cf9f02f993718b146ada28ff2369f72f
MrCoder <eagle.xiao@gmail.com>
2022-12-01 22:57:01 +1100

    Removed one cypress test that does not apply anymore.

--- cypress/integration/rendering/zenuml.spec.js
+++ cypress/integration/rendering/zenuml.spec.js
@@ -1,12 +1,6 @@
 import { imgSnapshotTest } from '../../helpers/util.js';
 
 describe('Zen UML', () => {
-  it('Only a Zen UML container with loaded css', () => {
-    imgSnapshotTest(`zenuml`, {});
-    cy.get('link[href="./style.css"]').should('exist');
-    cy.get('.zenuml').should('exist');
-  });
-
   it('Basic Zen UML diagram', () => {
     imgSnapshotTest(
       `
