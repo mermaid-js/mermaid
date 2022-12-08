6c2647e8cf79c5c5a3d57ef3ebff036a38773e30
Knut Sveidqvist <knsv@sveido.com>
2022-12-08 16:39:21 +0100

    Update url snapshot test for external diagrams

--- cypress/integration/other/external-diagrams.spec.js
+++ cypress/integration/other/external-diagrams.spec.js
@@ -5,12 +5,6 @@ describe('mermaid', () => {
     it('should work on @mermaid-js/mermaid-mindmap and mermaid-example-diagram', () => {
       const url = 'http://localhost:9000/external-diagrams-mindmap.html';
       urlSnapshotTest(url, {}, false, false);
-      // cy.visit(url);
-
-      // cy.get('svg', {
-      //   // may be a bit slower than normal, since vite might need to re-compile mermaid/mermaid-mindmap/mermaid-example-diagram
-      //   timeout: 10000,
-      // }).matchImageSnapshot();
     });
   });
 });
