56e28a7f405a69827790397479c9d5485d0174eb
Tom PERRILLAT-COLLOMB <tom.perrillat-collomb@protonmail.com>
2022-12-16 22:35:20 +0000

    test(generic): add generic type unit test

--- packages/mermaid/src/diagrams/common/common.spec.js
+++ packages/mermaid/src/diagrams/common/common.spec.js
@@ -69,5 +69,6 @@ describe('generic parser', function () {
       'test <Array<Array<string[]>>>'
     );
     expect(parseGenericTypes('~test')).toEqual('~test');
+    expect(parseGenericTypes('~test Array~string~')).toEqual('~test Array<string>');
   });
 });
