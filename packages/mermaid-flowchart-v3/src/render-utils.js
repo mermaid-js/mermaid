e28a766e7df82e7a3be73f7ba77a3067c502a793
Knut Sveidqvist <knsv@sveido.com>
2023-01-11 19:56:04 +0100

    Cleanup, updating the module name and description for flowchart in package.json

--- packages/mermaid-flowchart-v3/src/render-utils.js
+++ packages/mermaid-flowchart-v3/src/render-utils.js
@@ -1,43 +1,3 @@
-export const findCommonAncestorCoPilot = (id1, id2, treeData) => {
-  const { parentById, childrenById } = treeData;
-  const parents1 = [];
-  const parents2 = [];
-  let cnt = 0;
-  let currentId = id1;
-  while (currentId) {
-    parents1.push(currentId);
-    currentId = parentById[currentId];
-    cnt++;
-    if (cnt > 200) {
-      throw new Error('Infinite loop detected!');
-    }
-  }
-  currentId = id2;
-  while (currentId) {
-    parents2.push(currentId);
-    currentId = parentById[currentId];
-    cnt++;
-    if (cnt > 200) {
-      throw new Error('Infinite loop detected!');
-    }
-  }
-  let commonAncestor = 'root';
-  while (parents1.length && parents2.length) {
-    cnt++;
-    if (cnt > 200) {
-      throw new Error('Infinite loop detected!');
-    }
-    const p1 = parents1.pop();
-    const p2 = parents2.pop();
-    if (p1 === p2) {
-      commonAncestor = p1;
-    } else {
-      break;
-    }
-  }
-  return commonAncestor;
-};
-
 export const findCommonAncestor = (id1, id2, treeData) => {
   const { parentById } = treeData;
   const visited = new Set();
@@ -55,65 +15,6 @@ export const findCommonAncestor = (id1, id2, treeData) => {
       return currentId;
     }
     currentId = parentById[currentId];
-  }
-  return 'root';
-};
-
-export const findCommonAncestorKnut = (id1, id2, treeData) => {
-  const { parentById, childrenById } = treeData;
-  const parents1 = [];
-  const parents2 = [];
-  let cnt = 0;
-  let currentId = id1;
-  while (currentId) {
-    parents1.push(currentId);
-    currentId = parentById[currentId];
-    cnt++;
-    if (cnt > 200) {
-      throw new Error('Infinite loop detected!');
-    }
-  }
-  currentId = id2;
-  while (currentId) {
-    parents2.push(currentId);
-    currentId = parentById[currentId];
-    if (currentId === 'root') {
-      return 'root';
-    }
-
-    if (parents1.includes(currentId)) {
-      return currentId;
-    }
-
-    cnt++;
-    if (cnt > 200) {
-      throw new Error('Infinite loop detected!');
-    }
-  }
-  return 'root';
-};
-
-export const findCommonAncestorRecursive = (id1, id2, treeData) => {
-  const { parentById, childrenById } = treeData;
-
-  // Base case: return the current node if it is the common ancestor
-  if (id1 === id2) {
-    return id1;
-  }
-
-  // Recursive case: search for the common ancestor in the parent nodes
-  const parent1 = parentById[id1];
-  const parent2 = parentById[id2];
-  if (parent1 && parent2) {
-    return findCommonAncestor(parent1, parent2, treeData);
-  }
-
-  // Edge case: one of the nodes is the root of the tree
-  if (parent1) {
-    return parent1;
-  }
-  if (parent2) {
-    return parent2;
   }
   return 'root';
 };
--- packages/mermaid-flowchart-v3/src/render-utils.spec.js
+++ packages/mermaid-flowchart-v3/src/render-utils.spec.js
@@ -2,21 +2,6 @@ import { findCommonAncestor } from './render-utils';
 describe('when rendering a flowchart using elk ', function () {
   let lookupDb;
   beforeEach(function () {
-    /**
-     * root:
-     *   B1
-     *   outer
-     *     B6
-     *   Ugge
-     *     B2
-     *     B3
-     *     inner
-     *       B4
-     *       B5
-     *     inner2
-     *       C4
-     *       C5
-     */
     lookupDb = JSON.parse(
       '{"parentById":{"B4":"inner","B5":"inner","C4":"inner2","C5":"inner2","B2":"Ugge","B3":"Ugge","inner":"Ugge","inner2":"Ugge","B6":"outer"},"childrenById":{"inner":["B4","B5"],"inner2":["C4","C5"],"Ugge":["B2","B3","inner","inner2"],"outer":["B6"]}}'
     );
