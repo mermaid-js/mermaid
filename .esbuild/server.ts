192aac388e8b8d4244b5cd789a3d67e4d72bdeba
Sidharth Vinod <sidharthv96@gmail.com>
2023-02-19 21:59:49 +0530

    Fix external diagram test

--- .esbuild/server.ts
+++ .esbuild/server.ts
@@ -1,21 +1,23 @@
 import express from 'express';
 import cors from 'cors';
-import proxy from 'express-http-proxy';
 import { getBuildConfig } from './util.js';
 import { context } from 'esbuild';
 
 async function createServer() {
   const app = express();
-  const config = getBuildConfig({ minify: false, core: false, entryName: 'mermaid' });
-  const ctx = await context(config);
-  ctx.watch();
-  let { host, port } = await ctx.serve({ servedir: './dist' });
+  const mermaidCtx = await context(
+    getBuildConfig({ minify: false, core: false, entryName: 'mermaid' })
+  );
+  mermaidCtx.watch();
+  const externalCtx = await context(
+    getBuildConfig({ minify: false, core: false, entryName: 'mermaid-example-diagram' })
+  );
+  externalCtx.watch();
   app.use(cors());
   app.use(express.static('./packages/mermaid/dist'));
   app.use(express.static('./packages/mermaid-example-diagram/dist'));
   app.use(express.static('demos'));
   app.use(express.static('cypress/platform'));
-  app.use('/', proxy(`http://${host}:${port}`));
 
   app.listen(9000, () => {
     console.log(`Listening on http://localhost:9000`);
