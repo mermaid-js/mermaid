221640aa258c9e8f97e8113a15a52097cb2d1cf8
Justin Greywolf <jgreywolf@users.noreply.github.com>
2023-05-03 17:01:27 -0700

    Fix path in test

--- packages/mermaid/src/diagrams/class/classDiagramGrammar.spec.ts
+++ packages/mermaid/src/diagrams/class/classDiagramGrammar.spec.ts
@@ -4,12 +4,12 @@ import { LALRGenerator } from 'jison';
 import path from 'path';
 
 const getAbsolutePath = (relativePath: string) => {
-  return new URL(path.join(path.dirname(import.meta.url), relativePath)).pathname;
+  return new URL(path.join(__dirname, relativePath)).pathname;
 };
 
 describe('class diagram grammar', function () {
   it('should have no conflicts', async function () {
-    const grammarSource = await readFile(getAbsolutePath('./parser/classDiagram.jison'), 'utf8');
+    const grammarSource = await readFile(getAbsolutePath('parser/classDiagram.jison'), 'utf8');
     const grammarParser = new LALRGenerator(grammarSource, {});
     expect(grammarParser.conflicts).toBe(0);
   });
