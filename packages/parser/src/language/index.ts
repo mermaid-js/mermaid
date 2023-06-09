33a37392b906e0f3c5f0a2d7b01ddf32c87b4a42
Yokozuna59 <u.yokozuna@gmail.com>
2023-06-09 22:22:47 +0300

    export modules and services in parser package

--- packages/parser/src/language/index.ts
+++ packages/parser/src/language/index.ts
@@ -2,7 +2,5 @@ export * from './generated/ast.js';
 export * from './generated/grammar.js';
 export * from './generated/module.js';
 
-export * from './mermaidModule.js';
-export * from './mermaidLexer.js';
-export * from './mermaidTokenBuilder.js';
-export * from './mermaidValueConverter.js';
+export * from './modules/index.js';
+export * from './services/index.js';
---
+++ packages/parser/src/language/services/index.ts
@@ -0,0 +1,3 @@
+export * from './token-builders/index.js';
+export * from './value-converters/index.js';
+export * from './mermaidLexer.js';
