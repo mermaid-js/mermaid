6af2438474cf8601110a6d6bd36f51a6a2b82d94
Yokozuna59 <u.yokozuna@gmail.com>
2023-06-14 13:48:39 +0300

    add default info db and recreate clear function for info

--- packages/mermaid/src/diagrams/info/infoDb.ts
+++ packages/mermaid/src/diagrams/info/infoDb.ts
@@ -1,13 +1,16 @@
-import { clear } from '../../commonDb.js';
-import type { InfoDB } from './infoTypes.js';
+import { DEFAULT_INFO_DB, type InfoDB } from './infoTypes.js';
 
-let info = false;
+let info: boolean = DEFAULT_INFO_DB.info;
 
 export const setInfo = (toggle: boolean): void => {
   info = toggle;
 };
 
 export const getInfo = (): boolean => info;
+
+const clear = (): void => {
+  info = DEFAULT_INFO_DB.info;
+};
 
 const db: InfoDB = {
   clear,
--- packages/mermaid/src/diagrams/info/infoTypes.ts
+++ packages/mermaid/src/diagrams/info/infoTypes.ts
@@ -1,5 +1,9 @@
 import type { DiagramDB } from '../../diagram-api/types.js';
 
+export const DEFAULT_INFO_DB = {
+  info: false,
+} as const;
+
 export interface InfoDB extends DiagramDB {
   clear: () => void;
   setInfo: (info: boolean) => void;
