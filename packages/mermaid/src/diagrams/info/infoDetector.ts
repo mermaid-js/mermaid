95bba56bd82fc4b28df5bcae495e1c2e7fcb65f3
Yokozuna59 <u.yokozuna@gmail.com>
2023-06-16 00:07:06 +0300

    rename plugin variable into info in infoDetector.ts

--- packages/mermaid/src/diagrams/info/infoDetector.ts
+++ packages/mermaid/src/diagrams/info/infoDetector.ts
@@ -15,10 +15,8 @@ const loader: DiagramLoader = async () => {
   return { id, diagram };
 };
 
-const plugin: ExternalDiagramDefinition = {
+export const info: ExternalDiagramDefinition = {
   id,
   detector,
   loader,
 };
-
-export const info = plugin;
