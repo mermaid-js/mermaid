5aba2fed8b408d3900d657897c4ac7afdd276ae5
Yokozuna59 <u.yokozuna@gmail.com>
2023-06-17 00:01:54 +0300

    remove cy.get of info diagram

--- cypress/integration/rendering/info.spec.ts
+++ cypress/integration/rendering/info.spec.ts
@@ -3,11 +3,9 @@ import { imgSnapshotTest } from '../../helpers/util.js';
 describe('info diagram', () => {
   it('should handle an info definition', () => {
     imgSnapshotTest(`info`);
-    cy.get('svg');
   });
 
   it('should handle an info definition with showInfo', () => {
     imgSnapshotTest(`info showInfo`);
-    cy.get('svg');
   });
 });
