db2a556f626beb630629f9ddc39e604590eb2610
Nikolay Rozhkov <nironame@gmail.com>
2023-06-25 00:23:42 +0300

    Cleanup

--- cypress/integration/rendering/sankey.spec.js
+++ cypress/integration/rendering/sankey.spec.js
@@ -1,4 +1,4 @@
-import { imgSnapshotTest, renderGraph } from '../../helpers/util.js';
+import { imgSnapshotTest } from '../../helpers/util.js';
 
 describe('Sankey Diagram', () => {
   it('should render a simple example', () => {
