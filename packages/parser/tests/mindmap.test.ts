d54ad15e8f64f326f4c89c49386ec174a511305b
Reda Al Sulais <u.yokozuna@gmail.com>
2023-07-30 17:06:28 +0300

    add `nodes` and `decorations` test cases for `mindmap`

--- packages/parser/tests/mindmap.test.ts
+++ packages/parser/tests/mindmap.test.ts
@@ -11,8 +11,7 @@ id1[awesome title]
   :::urgent large
 ::icon(fa fa-book)
 
-  id2[awesome title 2]
-`;
+  id2[awesome title 2]`;
     const result = parse(context);
     expect(result.parserErrors).toHaveLength(0);
     expect(result.lexerErrors).toHaveLength(0);
@@ -27,5 +26,157 @@ id1[awesome title]
 
     expect(value.root.children[0].id).toBe('id2');
     expect(value.root.children[0].title).toBe('awesome title 2');
+  });
+
+  describe('nodes', () => {
+    it('should handle rectangle node definition', () => {
+      const context = `mindmap
+      root[the root]`;
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('rectangle');
+    });
+
+    it('should handle rounded rectangle node definition', () => {
+      const context = `mindmap
+      root(the root)`;
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('rounded-rectangle');
+    });
+
+    it('should handle circle node definition', () => {
+      const context = `mindmap
+      root((the root))`;
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('circle');
+    });
+
+    it('should handle cloud node definition', () => {
+      const context = `mindmap
+      root)the root(`;
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('cloud');
+    });
+
+    it('should handle bang node definition', () => {
+      const context = `mindmap
+      root)the root(`;
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('bang');
+    });
+
+    it('should handle hexagon node definition', () => {
+      const context = `mindmap
+      root)the root(`;
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('hexagon');
+    });
+  });
+
+  describe('decorations', () => {
+    it('should handle icons for the node', () => {
+      const context = `mindmap
+      root[the root]
+      ::icon(bomb)`;
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('rectangle');
+      expect(value.root.icon).toEqual('bomb');
+    });
+
+    it('should handle classes for the node', () => {
+      const context = `mindmap
+      root[the root]
+      :::m-4 p-8`;
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('rectangle');
+      expect(value.root.class).toEqual('m-4 p-8');
+    });
+
+    it.each([
+      `mindmap
+      root[the root]
+      ::icon(bomb)
+      :::m-4 p-8`,
+      `mindmap
+      root[the root]
+      :::m-4 p-8
+      ::icon(bomb)`,
+    ])('should handle both classes and icons for the node', (context: string) => {
+      const result = parse(context);
+      expect(result.parserErrors).toHaveLength(0);
+      expect(result.lexerErrors).toHaveLength(0);
+
+      const value = result.value;
+      expect(value.$type).toBe(Mindmap);
+
+      expect(value.root.id).toBe('root');
+      expect(value.root.title).toBe('the root');
+      // expect(value.root.type).toBe('rectangle');
+      expect(value.root.icon).toEqual('bomb');
+      expect(value.root.class).toEqual('m-4 p-8');
+    });
   });
 });
