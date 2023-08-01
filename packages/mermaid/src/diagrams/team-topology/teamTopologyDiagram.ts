fcccce223fd33eaf334c161577a493306293571b
Brian Graham <379322+Incognito@users.noreply.github.com>
2023-08-01 11:25:57 +0200

    Update packages/mermaid/src/diagrams/team-topology/teamTopologyDiagram.ts
    Co-authored-by: Sidharth Vinod <sidharthv96@gmail.com>

--- packages/mermaid/src/diagrams/team-topology/teamTopologyDiagram.ts
+++ packages/mermaid/src/diagrams/team-topology/teamTopologyDiagram.ts
@@ -1,5 +1,5 @@
 import { DiagramDefinition } from '../../diagram-api/types.js';
-// @ts-ignore: TODO Fix ts errors
+// @ts-ignore: JISON doesn't support types.
 import parser from './parser/teamTopology.jison';
 import db from './teamTopologyDb.js';
 import styles from './styles.js';
