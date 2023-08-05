a96024f11b4f0d11e753ec39d2962dc3f80de159
Reda Al Sulais <u.yokozuna@gmail.com>
2023-08-05 15:05:43 +0300

    add `DiagramDefinition` to timelineDiagram

--- packages/mermaid/src/diagrams/timeline/timelineDiagram.ts
+++ packages/mermaid/src/diagrams/timeline/timelineDiagram.ts
@@ -1,10 +1,11 @@
+import type { DiagramDefinition } from '../../diagram-api/types.js';
 // @ts-ignore: JISON doesn't support types
 import parser from './parser/timeline.jison';
 import * as db from './timelineDb.js';
 import renderer from './timelineRenderer.js';
 import styles from './timelineStyles.js';
 
-export const diagram = {
+export const diagram: DiagramDefinition = {
   db,
   renderer,
   parser,
