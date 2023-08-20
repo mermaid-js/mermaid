83e47a7216cdb82373df1efc14ca630bb5b0c644
Reda Al Sulais <u.yokozuna@gmail.com>
2023-08-21 02:54:30 +0300

    fix: use `execFileSync` instead of `execSync` in `generateLangium`

--- .build/generateLangium.ts
+++ .build/generateLangium.ts
@@ -1,5 +1,11 @@
-import { execSync } from 'child_process';
+import { execFileSync } from 'child_process';
 
 export function generateLangium() {
-  execSync(`pnpm --prefix ${process.cwd()}/packages/parser exec langium generate`);
+  execFileSync('pnpm', [
+    '--prefix',
+    `${process.cwd()}/packages/parser`,
+    'exec',
+    'langium',
+    'generate',
+  ]);
 }
