eb63568ceb76854c89ccf79e58c31e6567c2962f
Reda Al Sulais <u.yokozuna@gmail.com>
2023-08-21 03:04:39 +0300

    chore: refactore `&&` into `if` in `populateCommonDb`

--- packages/mermaid/src/diagrams/common/populateCommonDb.ts
+++ packages/mermaid/src/diagrams/common/populateCommonDb.ts
@@ -3,7 +3,13 @@ import type { DiagramAST } from 'mermaid-parser';
 import type { DiagramDB } from '../../diagram-api/types.js';
 
 export function populateCommonDb(ast: DiagramAST, db: DiagramDB) {
-  ast.accDescr && db.setAccDescription?.(ast.accDescr);
-  ast.accTitle && db.setAccTitle?.(ast.accTitle);
-  ast.title && db.setDiagramTitle?.(ast.title);
+  if (ast.accDescr) {
+    db.setAccDescription?.(ast.accDescr);
+  }
+  if (ast.accTitle) {
+    db.setAccTitle?.(ast.accTitle);
+  }
+  if (ast.title) {
+    db.setDiagramTitle?.(ast.title);
+  }
 }
