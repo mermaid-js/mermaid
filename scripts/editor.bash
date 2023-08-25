1eee905f3b96d20f2fdc2a03d734c203f0811560
Sidharth Vinod <sidharthv96@gmail.com>
2023-08-25 09:59:45 +0530

    debug

--- scripts/editor.bash
+++ scripts/editor.bash
@@ -4,20 +4,17 @@ set -euxo pipefail
 # We have to use npm instead of yarn because it causes trouble in netlify
 pnpm build
 
-# Link local mermaid to npm
-pushd packages/mermaid
-npm link
-popd
-
 # Clone the Mermaid Live Editor repository
 rm -rf mermaid-live-editor
 git clone --single-branch https://github.com/mermaid-js/mermaid-live-editor.git
 
-# Link local mermaid to live editor
-npm link mermaid
+cd mermaid-live-editor
 
 # Install dependencies
-npm install --force
+npm install
+
+# Link local mermaid to live editor
+npm link ../packages/mermaid     
 
 # Force Build the site
 npm run build -- --force
