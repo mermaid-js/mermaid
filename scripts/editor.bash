32d178339044273a498d0b30d22db342b70314a0
Sidharth Vinod <sidharthv96@gmail.com>
2023-08-25 08:22:24 +0530

    chore: Update editor script
    Co-authored-by: Alois Klink <alois@aloisklink.com>

--- scripts/editor.bash
+++ scripts/editor.bash
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
 # We have to use npm instead of yarn because it causes trouble in netlify
 
 # Link local mermaid to npm
-pushd packages/mermaid || exit
+pushd packages/mermaid
 npm link
-popd || exit
+popd
 
 # Clone the Mermaid Live Editor repository
 git clone https://github.com/mermaid-js/mermaid-live-editor.git
 
 # Change to the repository directory
-cd mermaid-live-editor || exit
+cd mermaid-live-editor
 
 # Link local mermaid to live editor
 npm link mermaid
