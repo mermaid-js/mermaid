a6bb1ea28ce6700bb149c674b1ea3a5b4368ff4d
Sidharth Vinod <sidharthv96@gmail.com>
2023-10-04 14:09:34 +0530

    Fix TopBar

--- packages/mermaid/src/docs/.vitepress/components/TopBar.vue
+++ packages/mermaid/src/docs/.vitepress/components/TopBar.vue
@@ -1,19 +1,14 @@
 <template>
   <div
-    class="w-full top-bar bg-gradient-to-r from-[#bd34fe] to-[#ff3670] flex items-center justify-center p-1"
+    class="w-full top-bar bg-gradient-to-r from-[#bd34fe] to-[#ff3670] flex items-center text-center justify-center p-1 text-white"
   >
-    We've made our
     <a
       href="https://www.producthunt.com/posts/mermaid-chart?utm_source=badge-featured&amp;utm_medium=badge&amp;utm_souce=badge-mermaid-chart"
       target="_blank"
-      class="unstyled font-semibold text-lg underline px-1"
-      >Product Hunt</a
     >
-    debut! &nbsp;
-    <a
-      href="https://www.producthunt.com/posts/mermaid-chart?utm_source=badge-featured&amp;utm_medium=badge&amp;utm_souce=badge-mermaid-chart"
-      target="_blank"
-      >Show us some love and help spread the word</a
-    >, plus receive 25% off on annual Pro subscription!
+      We've made our Product Hunt debut! &nbsp;
+      <span class="underline">Show us some love and help spread the word</span>, plus receive 25%
+      off on annual Pro subscription!</a
+    >
   </div>
 </template>
