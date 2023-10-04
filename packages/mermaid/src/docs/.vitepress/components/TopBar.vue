d17c75222935fc26d60c6547ae2d8ea1db3fe5f5
Sidharth Vinod <sidharthv96@gmail.com>
2023-10-04 14:06:43 +0530

    Add TopBar

---
+++ packages/mermaid/src/docs/.vitepress/components/TopBar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div
+    class="w-full top-bar bg-gradient-to-r from-[#bd34fe] to-[#ff3670] flex items-center justify-center p-1"
+  >
+    We've made our
+    <a
+      href="https://www.producthunt.com/posts/mermaid-chart?utm_source=badge-featured&amp;utm_medium=badge&amp;utm_souce=badge-mermaid-chart"
+      target="_blank"
+      class="unstyled font-semibold text-lg underline px-1"
+      >Product Hunt</a
+    >
+    debut! &nbsp;
+    <a
+      href="https://www.producthunt.com/posts/mermaid-chart?utm_source=badge-featured&amp;utm_medium=badge&amp;utm_souce=badge-mermaid-chart"
+      target="_blank"
+      >Show us some love and help spread the word</a
+    >, plus receive 25% off on annual Pro subscription!
+  </div>
+</template>
