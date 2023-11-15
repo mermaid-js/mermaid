e86f09aaedce59af5c4edc890fcd1a11dc04432f
Reda Al Sulais <u.yokozuna@gmail.com>
2023-11-15 12:48:16 +0300

    Apply suggested style from code review
    Co-authored-by: Sidharth Vinod <sidharthv96@gmail.com>

--- packages/mermaid/src/diagrams/pie/pieParser.ts
+++ packages/mermaid/src/diagrams/pie/pieParser.ts
@@ -1,17 +1,16 @@
 import type { Pie } from 'mermaid-parser';
 import { parse } from 'mermaid-parser';
-
 import { log } from '../../logger.js';
 import type { ParserDefinition } from '../../diagram-api/types.js';
 import { populateCommonDb } from '../common/populateCommonDb.js';
 import type { PieDB } from './pieTypes.js';
 import { db } from './pieDb.js';
 
-function populateDb(ast: Pie, db: PieDB) {
+const populateDb = (ast: Pie, db: PieDB) => {
   populateCommonDb(ast, db);
   db.setShowData(ast.showData);
   ast.sections.map(db.addSection);
-}
+};
 
 export const parser: ParserDefinition = {
   parse: (input: string): void => {
