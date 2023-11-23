9ae00ebcdb7541cbb039c06af175a5c00e96cb83
Omer Priel <omer.priel2@gmail.com>
2023-11-23 14:55:09 +0200

    reset the testTimeout to 5 seconds and change it directly in the test

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,6 @@ export default defineConfig({
       reportsDirectory: './coverage/vitest',
       exclude: ['**/node_modules/**', '**/tests/**', '**/__mocks__/**'],
     },
-    testTimeout: 10000,
   },
   build: {
     /** If you set esmExternals to true, this plugins assumes that
