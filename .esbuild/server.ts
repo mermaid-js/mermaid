cbcd2cce9bffbddd8e80f6b6370e91c5cdd602cf
Sidharth Vinod <sidharthv96@gmail.com>
2023-11-24 10:38:32 +0530

    chore: Optimize dev server

--- .esbuild/server.ts
+++ .esbuild/server.ts
@@ -7,37 +7,23 @@ import chokidar from 'chokidar';
 import { generateLangium } from '../.build/generateLangium.js';
 import { packageOptions } from '../.build/common.js';
 
-const parserCtx = await context(
-  getBuildConfig({ ...defaultOptions, minify: false, core: false, entryName: 'parser' })
+const configs = Object.values(packageOptions).map(({ packageName }) =>
+  getBuildConfig({ ...defaultOptions, minify: false, core: false, entryName: packageName })
 );
-const mermaidCtx = await context(
-  getBuildConfig({ ...defaultOptions, minify: false, core: false, entryName: 'mermaid' })
-);
-const mermaidIIFECtx = await context(
-  getBuildConfig({
-    ...defaultOptions,
-    minify: false,
-    core: false,
-    entryName: 'mermaid',
-    format: 'iife',
-  })
-);
-const externalCtx = await context(
-  getBuildConfig({
-    ...defaultOptions,
-    minify: false,
-    core: false,
-    entryName: 'mermaid-example-diagram',
-  })
-);
-const zenumlCtx = await context(
-  getBuildConfig({ ...defaultOptions, minify: false, core: false, entryName: 'mermaid-zenuml' })
-);
-const contexts = [parserCtx, mermaidCtx, mermaidIIFECtx, externalCtx, zenumlCtx];
+const mermaidIIFEConfig = getBuildConfig({
+  ...defaultOptions,
+  minify: false,
+  core: false,
+  entryName: 'mermaid',
+  format: 'iife',
+});
+configs.push(mermaidIIFEConfig);
+
+const contexts = await Promise.all(configs.map((config) => context(config)));
 
 const rebuildAll = async () => {
   console.time('Rebuild time');
-  await Promise.all(contexts.map((ctx) => ctx.rebuild()));
+  await Promise.all(contexts.map((ctx) => ctx.rebuild())).catch((e) => console.error(e));
   console.timeEnd('Rebuild time');
 };
 
