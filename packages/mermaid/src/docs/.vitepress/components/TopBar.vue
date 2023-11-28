af9566df75f897d5e304cc8b9f2e27e119903724
Sidharth Vinod <sidharthv96@gmail.com>
2023-11-28 11:42:04 +0530

    Update packages/mermaid/src/docs/.vitepress/components/TopBar.vue

--- packages/mermaid/src/docs/.vitepress/components/TopBar.vue
+++ packages/mermaid/src/docs/.vitepress/components/TopBar.vue
@@ -8,7 +8,6 @@
       <strong>25%</strong> off.<a
         href="https://www.mermaidchart.com/holiday-promo"
         target="_blank"
-        rel="noopener noreferrer"
         class="unstyled ml-2 text-white underline"
         >View more details here.</a
       >
