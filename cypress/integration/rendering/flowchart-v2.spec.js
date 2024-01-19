d6e738ac4cf2e208326b0ca3a8702d940cb846ec
Sidharth Vinod <sidharthv96@gmail.com>
2024-01-19 12:21:51 +0530

    Run all tests

--- cypress/integration/rendering/flowchart-v2.spec.js
+++ cypress/integration/rendering/flowchart-v2.spec.js
@@ -6,6 +6,7 @@ describe('Flowchart v2', () => {
       `flowchart TD
       A[Christmas] -->|Get money| B(Go shopping)
       B --> C{Let me think}
+      C -->|One| D[Laptop]
       C -->|Two| E[iPhone]
       C -->|Three| F[fa:fa-car Car]
       `,
