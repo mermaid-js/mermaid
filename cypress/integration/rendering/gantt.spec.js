c1b5d527df06a7305401749ab327fac3a7b4c59b
Franck Zagala <franckzagala@gmail.com>
2024-01-27 13:24:12 +0000

    modify invalid e2e test for gantt chart

--- cypress/integration/rendering/gantt.spec.js
+++ cypress/integration/rendering/gantt.spec.js
@@ -14,19 +14,18 @@ describe('Gantt diagram', () => {
       excludes weekdays 2014-01-10
 
       section A section
-      Completed task            :done,    des1, 2014-01-06,2014-01-08
-      Active task               :active,  des2, 2014-01-09, 3d
-      Future task               :         des3, after des2, 5d
-      Future task2               :         des4, after des3, 5d
+      Completed task                      :done, des1, 2014-01-06,2014-01-08
+      Active task                         :active, des2, 2014-01-09, 3d
+      Future task                         :des3, after des2, 5d
+      Future task2                        :des4, after des3, 5d
+      Waiting for task 2                  :after des1, until des4
 
       section Critical tasks
       Completed task in the critical line :crit, done, 2014-01-06,24h
-      Implement parser and jison :crit, done, after des1, 2d
+      Implement parser and jison          :crit, done, after des1, 2d
       Create tests for parser             :crit, active, 3d
       Future task in critical line        :crit, 5d
       Create tests for renderer           :2d
-      Add to mermaid                      :adding, 1d
-      Thinking about adding to mermaid    :after des1, until adding
 
       section Documentation
       Describe gantt syntax               :active, a1, after des1, 3d
