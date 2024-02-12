bb84e17d66ebf513d70d955a95c8e2cb3b9ae3a1
Reda Al Sulais <u.yokozuna@gmail.com>
2024-02-12 23:39:10 +0300

    change ISankeyLink into SankeyLink in sankeyDB

--- packages/mermaid/src/diagrams/sankey/sankeyDB.ts
+++ packages/mermaid/src/diagrams/sankey/sankeyDB.ts
@@ -8,20 +8,20 @@ import {
   clear as commonClear,
 } from '../common/commonDb.js';
 import type { SankeyDiagramConfig } from '../../config.type.js';
-import type { SankeyDB, SankeyLink as ISankeyLink, SankeyFields } from './sankeyTypes.js';
+import type { SankeyDB, SankeyLink, SankeyFields } from './sankeyTypes.js';
 import DEFAULT_CONFIG from '../../defaultConfig.js';
 import type { RequiredDeep } from 'type-fest';
 
 export const DEFAULT_SANKEY_CONFIG: Required<SankeyDiagramConfig> = DEFAULT_CONFIG.sankey;
 
 export const DEFAULT_SANKEY_DB: RequiredDeep<SankeyFields> = {
-  links: [] as ISankeyLink[],
+  links: [] as SankeyLink[],
   nodes: [] as string[],
   config: DEFAULT_SANKEY_CONFIG,
 } as const;
 
 // Sankey diagram represented by nodes and links between those nodes
-let links: ISankeyLink[] = DEFAULT_SANKEY_DB.links;
+let links: SankeyLink[] = DEFAULT_SANKEY_DB.links;
 // Array of nodes guarantees their order
 let nodes: string[] = DEFAULT_SANKEY_DB.nodes;
 const config: Required<SankeyDiagramConfig> = structuredClone(DEFAULT_SANKEY_CONFIG);
@@ -39,11 +39,11 @@ const clear = (): void => {
  * @param target - Node where the link ends
  * @param value - Describes the amount to be passed
  */
-const addLink = ({ source, target, value }: ISankeyLink): void => {
+const addLink = ({ source, target, value }: SankeyLink): void => {
   links.push({ source, target, value });
 };
 
-const getLinks = (): ISankeyLink[] => links;
+const getLinks = (): SankeyLink[] => links;
 
 const addNode = (node: string): void => {
   nodes.push(node);
