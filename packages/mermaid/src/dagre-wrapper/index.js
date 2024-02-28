0ce33cb1601dacc05c6d77f308f4df04f69c6f3f
rowanfr <beanfun1@gmail.com>
2024-02-28 13:55:21 -0600

    Update packages/mermaid/src/dagre-wrapper/index.js
    The code setting the `ranksep` and `nodesep` to subgraphs was made cleaner and more legible while maintaining the intended functionality of subgraph spacing
    
    Co-authored-by: Alois Klink <alois@aloisklink.com>

--- packages/mermaid/src/dagre-wrapper/index.js
+++ packages/mermaid/src/dagre-wrapper/index.js
@@ -56,7 +56,12 @@ const recursiveRender = async (_elem, graph, diagramtype, id, parentCluster, sit
         log.info('Cluster identified', v, node.width, graph.node(v));
         // node.graph.setGraph applies the graph configurations such as nodeSpacing to subgraphs as without this the default values would be used
         // We override the `rankdir` variable to allow for the subgraph's direction to override the graphs direction in the subgraph
-        node.graph.setGraph({ ...graph.graph(), rankdir: node.graph.graph().rankdir });
+        const { ranksep, nodesep } = graph.graph();
+        node.graph.setGraph({
+          ...node.graph.graph(),
+          ranksep,
+          nodesep,
+        });
         const o = await recursiveRender(
           nodes,
           node.graph,
