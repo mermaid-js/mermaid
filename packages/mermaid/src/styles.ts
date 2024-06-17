757f5c395c09c3fc7bcf0f86dfb855ba463809e1
Per Brolin <per@mermaidchart.com>
2024-06-17 08:47:44 +0200

    MC-1765: Added drop-shadow for circles and polygons

--- packages/mermaid/src/styles.ts
+++ packages/mermaid/src/styles.ts
@@ -73,7 +73,7 @@ const getStyles = (
     stroke: ${options.nodeBorder};
   }
 
-  [data-look="neo"].node rect {
+  [data-look="neo"].node rect,  [data-look="neo"].node circle, [data-look="neo"].node polygon {
     stroke: none;
     filter: drop-shadow( 1px 2px 2px rgba(185,185,185,1.0) );
   }
