959115990187ea4221e39d3dc578779d6ec5c1ea
Per Brolin <per@mermaidchart.com>
2024-06-17 20:40:48 +0200

    Corrected end-state for neo

--- packages/mermaid/src/styles.ts
+++ packages/mermaid/src/styles.ts
@@ -73,9 +73,24 @@ const getStyles = (
     stroke: ${options.nodeBorder};
   }
 
-  [data-look="neo"].node rect,  [data-look="neo"].node circle, [data-look="neo"].node polygon {
+  [data-look="neo"].node rect, [data-look="neo"].node polygon {
     stroke: none;
     filter: drop-shadow( 1px 2px 2px rgba(185,185,185,1.0) );
+    rx: 3;
+    ry: 3;
+  }
+
+  [data-look="neo"].node rect,  [data-look="neo"].node circle, [data-look="neo"].node polygon {
+    stroke: $(options.nodeBorder);
+    filter: drop-shadow( 1px 2px 2px rgba(185,185,185,1.0) );
+    rx: 3;
+    ry: 3;
+  }
+
+  [data-look="neo"].node circle{
+    stroke: $(options.nodeBorder);
+    filter: drop-shadow( 1px 2px 2px rgba(185,185,185,1.0) );
+    fill: #000000;
     rx: 3;
     ry: 3;
   }
