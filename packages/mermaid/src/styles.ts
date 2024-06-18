2007cab5ee0346d3b41f9ce078f7321b016e42d4
Ashish Jain <ashishjain0512@gmail.com>
2024-06-18 14:16:28 +0200

    Fix circle shape neo look for flowchart

--- packages/mermaid/src/styles.ts
+++ packages/mermaid/src/styles.ts
@@ -93,9 +93,15 @@ const getStyles = (
     stroke: $(options.nodeBorder);
     stroke: ${options.useGradient ? 'url(#gradient)' : options.nodeBorder};
     filter: drop-shadow( 1px 2px 2px rgba(185,185,185,1.0) );
-    fill: #000000;
+    //fill: #000000;
     // rx: 3;
     // ry: 3;
+  }
+
+  [data-look="neo"].node circle .state-start{
+   
+    fill: #000000;
+    
   }
 
   [data-look="neo"].statediagram-cluster rect {
