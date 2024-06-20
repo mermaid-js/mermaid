1a6632e3772f9378172baf01a21c575ea79638e2
Sidharth Vinod <sidharthv96@gmail.com>
2024-06-20 16:23:41 +0530

    temp: Test argos

--- cypress/integration/rendering/quadrantChart.spec.js
+++ cypress/integration/rendering/quadrantChart.spec.js
@@ -21,7 +21,7 @@ describe('Quadrant Chart', () => {
     quadrant-2 Need to promote
     quadrant-3 Re-evaluate
     quadrant-4 May be improved
-    Campaign A: [0.3, 0.6]
+    Campaign A: [0.3, 0.61]
     Campaign B: [0.45, 0.23]
     Campaign C: [0.57, 0.69]
     Campaign D: [0.78, 0.34]
@@ -57,7 +57,7 @@ describe('Quadrant Chart', () => {
     x-axis Low Reach --> High Reach
     y-axis Low Engagement --> High Engagement
     quadrant-1 We should expand
-    quadrant-2 Need to promote
+    quadrant-2 Need to promot
     quadrant-3 Re-evaluate
     quadrant-4 May be improved
       `,
