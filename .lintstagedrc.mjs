22a00a5f8bba6f30eb3d119498a4533fa1838920
Sidharth Vinod <sidharthv96@gmail.com>
2024-06-21 12:08:14 +0530

    chore: Update lint scripts

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,10 +1,6 @@
 export default {
   '!(docs/**/*)*.{ts,js,html,md,mts}': [
-    'eslint --cache --cache-strategy content --fix',
-    // don't cache prettier yet, since we use `prettier-plugin-jsdoc`,
-    // and prettier doesn't invalidate cache on plugin updates"
-    // https://prettier.io/docs/en/cli.html#--cache
-    'prettier --write',
+    'biome check --no-errors-on-unmatched --files-ignore-unknown=true',
   ],
   '.cspell/*.txt': ['tsx scripts/fixCSpell.ts'],
   '**/*.jison': ['pnpm -w run lint:jison'],
