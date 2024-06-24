f45ae227a0735b015974ed9a9bb27fb06ff67964
Ashish Jain <ashishjain0512@gmail.com>
2024-06-24 11:32:24 +0200

    #5237 Added hasKatex check

--- packages/mermaid/src/rendering-util/createText.ts
+++ packages/mermaid/src/rendering-util/createText.ts
@@ -8,7 +8,7 @@ import { markdownToHTML, markdownToLines } from '../rendering-util/handle-markdo
 import { decodeEntities } from '../utils.js';
 import { splitLineToFitWidth } from './splitText.js';
 import type { MarkdownLine, MarkdownWord } from './types.js';
-import common, { renderKatex } from '$root/diagrams/common/common.js';
+import common, { hasKatex, renderKatex, hasKatex } from '$root/diagrams/common/common.js';
 import { getConfig } from '$root/diagram-api/diagramAPI.js';
 
 function applyStyle(dom, styleFn) {
@@ -24,7 +24,7 @@ async function addHtmlSpan(element, node, width, classes, addBackground = false)
   // const label = node.label;
   let label = '';
 
-  if (node.label) {
+  if (node.label && hasKatex(node.label)) {
     label = await renderKatex(node.label.replace(common.lineBreakRegex, '\n'), getConfig());
   }
   const labelClass = node.isNode ? 'nodeLabel' : 'edgeLabel';
