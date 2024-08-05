64cc767dd5d714fdc178bb30ebb8dffb0f3cbe49
Alois Klink <alois@mermaidchart.com>
2024-08-05 23:05:55 +0900

    fix: add `mc` mermaid theme to types

--- packages/mermaid/src/config.type.ts
+++ packages/mermaid/src/config.type.ts
@@ -61,7 +61,7 @@ export interface MermaidConfig {
    * You may also use `themeCSS` to override this value.
    *
    */
-  theme?: 'default' | 'base' | 'dark' | 'forest' | 'neutral' | 'neo' | 'neo-dark' | 'null';
+  theme?: 'default' | 'base' | 'dark' | 'forest' | 'neutral' | 'neo' | 'neo-dark' | 'mc' | 'null';
   themeVariables?: any;
   themeCSS?: string;
   /**
