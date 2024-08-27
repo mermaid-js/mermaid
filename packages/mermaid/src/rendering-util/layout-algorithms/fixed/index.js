fd47e4eb2f5cdbc8056fc630c980da7e671ee901
Ashish Jain <ashishjain0512@gmail.com>
2024-08-27 15:54:31 +0200

    fix fixed layout

--- packages/mermaid/src/rendering-util/layout-algorithms/fixed/index.js
+++ packages/mermaid/src/rendering-util/layout-algorithms/fixed/index.js
@@ -241,7 +241,7 @@ const doRender = async (_elem, data4Layout, siteConfig, positions) => {
  * Render the graph
  * ###############################################################
  */
-export const render = async (data4Layout, svg, positions) => {
+export const render = async (data4Layout, svg, _internalHelpers, _algorithm, positions) => {
   const element = svg.select('g');
   // Org
   insertMarkers(element, data4Layout.markers, data4Layout.type, data4Layout.diagramId);
