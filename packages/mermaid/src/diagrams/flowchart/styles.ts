01bf7af360de6fa87dc33c9a365db2c0341230c0
saurabhg772244 <saurabh@mermaidchart.com>
2024-09-11 17:12:56 +0530

    updated styles

--- packages/mermaid/src/diagrams/flowchart/styles.ts
+++ packages/mermaid/src/diagrams/flowchart/styles.ts
@@ -165,10 +165,17 @@ const getStyles = (options: FlowChartStyleOptions) =>
   }
 
   .icon-shape {
+    background-color: ${options.edgeLabelBackground};
     p {
       background-color: ${options.edgeLabelBackground};
       padding: 2px;
     }
+    rect {
+      opacity: 0.5;
+      background-color: ${options.edgeLabelBackground};
+      fill: ${options.edgeLabelBackground};
+    }
+    text-align: center;
   }
 `;
 
