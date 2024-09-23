97dec4e4cbd849f2e7e17cbd7b6027becc7bbf79
Knut Sveidqvist <knsv@sveido.com>
2024-09-23 15:10:42 +0200

    Decreasing drop shadow for neo-dark

--- packages/mermaid/src/themes/theme-neo-dark.js
+++ packages/mermaid/src/themes/theme-neo-dark.js
@@ -52,7 +52,7 @@ class Theme {
     this.useGradient = true;
     this.gradientStart = '#0042eb';
     this.gradientStop = '#eb0042';
-    this.dropShadow = 'drop-shadow( 1px 2px 2px rgba(185,185,185,0.5))';
+    this.dropShadow = 'drop-shadow( 1px 2px 2px rgba(185,185,185,0.2))';
 
     /* Architecture Diagram variables */
     this.archEdgeColor = 'calculated';
