a7bcd7dee57442336b10af9f11e4e54c9c530edd
Nicholas Bollweg <nick.bollweg@gmail.com>
2025-03-17 15:32:52 -0500

    #6380: move requirements containment arrow to start

--- packages/mermaid/src/diagrams/requirement/requirementDb.ts
+++ packages/mermaid/src/diagrams/requirement/requirementDb.ts
@@ -328,7 +328,7 @@ export class RequirementDB implements DiagramDB {
         thickness: 'normal',
         type: 'normal',
         pattern: isContains ? 'normal' : 'dashed',
-        arrowTypeEnd: isContains ? 'requirement_contains' : 'requirement_arrow',
+        arrowTypeStart: isContains ? 'requirement_contains' : 'requirement_arrow',
         look: config.look,
       };
 
