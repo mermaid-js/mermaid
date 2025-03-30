9bbfb2154aa0c5f382d7f2d2cc33babb49ce217b
Ladislav Gazo <ladislav.gazo@gmail.com>
2025-03-30 21:24:01 +0200

    fix(eventmodeling): improved coverage of the `db` file

--- packages/mermaid/src/diagrams/eventmodeling/eventmodeling.spec.ts
+++ packages/mermaid/src/diagrams/eventmodeling/eventmodeling.spec.ts
@@ -8,6 +8,39 @@ import { parser } from './parser.js';
 
 const { clear } = db;
 
+const fixtureStateView = `eventmodeling
+
+
+tf 01 scn CartScreen
+tf 02 cmd AddItem
+tf 03 evt ItemAdded
+`;
+
+const fixtureStateChange = `eventmodeling
+
+tf 03 evt ItemAdded
+tf 02 rmo CartItems
+tf 04 scn CartScreen
+`;
+
+const fixtureTranslation = `eventmodeling
+
+tf 03 evt External.InventoryChanged
+tf 02 pcr InventoryProcessor
+tf 04 cmd ChangeInventory
+tf 05 evt Cart.InventoryChanged
+`;
+
+const fixtureMultipleRelations = `eventmodeling
+
+
+rf 02 evt CartCreated
+rf 03 evt ItemAdded
+rf 04 evt ItemRemoved
+rf 05 evt CartCleared
+tf 01 rmo CartScreen >f 02 >f 03 >f 04 >f 05
+`;
+
 describe('eventmodeling diagrams', () => {
   beforeEach(() => {
     clear();
@@ -19,6 +52,44 @@ describe('eventmodeling diagrams', () => {
     tf 02 cmd RunAction
     tf 03 evt ActionExecuted`;
     await expect(parser.parse(str)).resolves.not.toThrow();
+  });
+
+  it('should produce correct store state for State View', async () => {
+    await parser.parse(fixtureStateView);
+    const state = db.getState();
+
+    expect(state.boxes.length).toBe(3);
+    expect(state.sortedSwimlanesArray.length).toBe(3);
+    expect(state.relations.length).toBe(2);
+  });
+
+  it('should produce correct store state for State Change', async () => {
+    await parser.parse(fixtureStateChange);
+    const state = db.getState();
+
+    expect(state.boxes.length).toBe(3);
+    expect(state.sortedSwimlanesArray.length).toBe(3);
+    expect(state.relations.length).toBe(2);
+  });
+
+  it('should produce correct store state for Translation', async () => {
+    await parser.parse(fixtureTranslation);
+    const state = db.getState();
+
+    // console.log(state);
+    expect(state.boxes.length).toBe(4);
+    expect(state.sortedSwimlanesArray.length).toBe(4);
+    expect(state.relations.length).toBe(3);
+  });
+
+  it('should produce correct store state for Multiple relations', async () => {
+    await parser.parse(fixtureMultipleRelations);
+    const state = db.getState();
+
+    // console.log(state);
+    expect(state.boxes.length).toBe(5);
+    expect(state.sortedSwimlanesArray.length).toBe(2);
+    expect(state.relations.length).toBe(4);
   });
 
   // fails because it reports `html` method not found for D3 div element. That's weird as it works outside of tests
