da6937f47490cc4d869465578cdd4a8cbfa5825d
Sidharth Vinod <sidharthv96@gmail.com>
2025-04-08 10:08:06 +0530

    temp: Revert all changes is cypress folder

--- cypress/integration/rendering/examples.spec.ts
+++
@@ -1,7 +0,0 @@
-import { urlSnapshotTest } from '../../helpers/util.ts';
-
-describe('Examples', () => {
-  it('should render all examples', () => {
-    urlSnapshotTest('http://localhost:9000/examples.html');
-  });
-});
