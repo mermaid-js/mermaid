13f8549f813dab8536ab82d8ac6ad0a0f192abd9
shubham-mermaid <shubham@mermaidchart.com>
2025-06-10 10:12:06 +0530

    Added test cases for layer assignments, node ordering and assigning initial positions for ipsepcola layout

---
+++ packages/mermaid/src/rendering-util/layout-algorithms/ipsecCola/layerAssignment.spec.ts
@@ -0,0 +1,166 @@
+import * as d3 from 'd3';
+import { FlowDB } from '../../../diagrams/flowchart/flowDb.js';
+import flow from '../../../diagrams/flowchart/parser/flowParser.js';
+import { createGraphWithElements } from '../../createGraph.js';
+import { layerAssignment } from './layerAssignment.js';
+import type { D3Selection } from '../../../types.js';
+import type { Node } from '../../types.js';
+
+describe('layerAssignment', () => {
+  beforeEach(function () {
+    flow.parser.yy = new FlowDB();
+    flow.parser.yy.clear();
+  });
+
+  it('should correctly assign the layers to node', async () => {
+    const flowchart = `
+          flowchart LR
+            A --> B --> C
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    expect(layoutData.nodes.find((node: Node) => node.id === 'A').layer).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'B').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'C').layer).toEqual(3);
+  });
+  it('should correctly assign the layers to node', async () => {
+    const flowchart = `
+          flowchart LR
+            subgraph two
+              b1
+            end
+            subgraph three
+              c2
+            end
+            three --> two
+            two --> c2
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    expect(layoutData.nodes.find((node: Node) => node.id === 'two').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'b1').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'three').layer).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'c2').layer).toEqual(3);
+
+    // expect(graph.getNodeAttributes('B').layer).toEqual(2);
+    // expect(graph.getNodeAttributes('C').layer).toEqual(3);
+  });
+  it('should correctly assign the layers to node', async () => {
+    const flowchart = `
+          flowchart LR
+            subgraph one
+              a1[Iam a node with a super long label] -- I am a long label --> a2[I am another node with a mega long label]
+              a1 -- Another long label --> a2
+              a3 --> a1 & a2 & a3 & a4
+              a1 --> a4
+            end
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    expect(layoutData.nodes.find((node: Node) => node.id === 'one').layer).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a1').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a2').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a3').layer).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a4').layer).toEqual(2);
+  });
+  it('should correctly assign the layers to node', async () => {
+    const flowchart = `
+          flowchart TD
+        P1
+        P1 -->P1.5
+        subgraph P1.5
+          P2
+          P2.5(( A ))
+          P3
+        end
+        P2 --> P4
+        P3 --> P6
+        P1.5 --> P5
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P1').layer).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P1.5').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P2').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P2.5').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P3').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P4').layer).toEqual(3);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P5').layer).toEqual(3);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P6').layer).toEqual(3);
+  });
+  it('should correctly assign the layers to node', async () => {
+    const flowchart = `
+          flowchart
+        subgraph Z
+        subgraph X
+        a --> b
+        end
+        subgraph Y
+        c --> d
+        end
+        end
+        Y --> X
+        X --> P
+        P --> Y
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    expect(layoutData.nodes.find((node: Node) => node.id === 'Z').layer).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'Y').layer).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'X').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'b').layer).toEqual(3);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'c').layer).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'd').layer).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P').layer).toEqual(3);
+  });
+});
---
+++ packages/mermaid/src/rendering-util/layout-algorithms/ipsecCola/nodeOrdering.spec.ts
@@ -0,0 +1,180 @@
+import { FlowDB } from '../../../diagrams/flowchart/flowDb.js';
+import flow from '../../../diagrams/flowchart/parser/flowParser.js';
+import type { D3Selection } from '../../../types.js';
+import { createGraphWithElements } from '../../createGraph.js';
+import type { Node } from '../../types.js';
+import { layerAssignment } from './layerAssignment.js';
+import { assignNodeOrder } from './nodeOrdering.js';
+import * as d3 from 'd3';
+
+describe('nodeOrdering', () => {
+  beforeEach(function () {
+    flow.parser.yy = new FlowDB();
+    flow.parser.yy.clear();
+  });
+
+  it('should correctly assign the orders to node', async () => {
+    const flowchart = `
+          flowchart LR
+            A --> B --> C
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    // Call Node order Assignment for the graph
+    assignNodeOrder(1, layoutData);
+
+    expect(layoutData.nodes.find((node: Node) => node.id === 'A').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'B').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'C').order).toEqual(0);
+  });
+  it('should correctly assign the orders to node', async () => {
+    const flowchart = `
+          flowchart LR
+            subgraph two
+              b1
+            end
+            subgraph three
+              c2
+            end
+            three --> two
+            two --> c2
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    // Call Node order Assignment for the graph
+    assignNodeOrder(50, layoutData);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'two').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'b1').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'three').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'c2').order).toEqual(0);
+
+    // expect(graph.getNodeAttributes('B').layer).toEqual(2);
+    // expect(graph.getNodeAttributes('C').layer).toEqual(3);
+  });
+
+  it('should correctly assign the orders to node', async () => {
+    const flowchart = `
+          flowchart LR
+            subgraph one
+              a1[Iam a node with a super long label] -- I am a long label --> a2[I am another node with a mega long label]
+              a1 -- Another long label --> a2
+              a3 --> a1 & a2 & a3 & a4
+              a1 --> a4
+            end
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    // Call Node order Assignment for the graph
+    assignNodeOrder(50, layoutData);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'one').order).toEqual(0.75);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a1').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a2').order).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a3').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a4').order).toEqual(2);
+  });
+  it('should correctly assign the orders to node', async () => {
+    const flowchart = `
+          flowchart TD
+        P1
+        P1 -->P1.5
+        subgraph P1.5
+          P2
+          P2.5(( A ))
+          P3
+        end
+        P2 --> P4
+        P3 --> P6
+        P1.5 --> P5
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    // Call Node order Assignment for the graph
+    assignNodeOrder(50, layoutData);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P1').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P1.5').order).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P2').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P2.5').order).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P3').order).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P4').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P5').order).toEqual(2);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P6').order).toEqual(1);
+  });
+
+  it('should correctly assign the orders to node', async () => {
+    const flowchart = `
+      flowchart
+        subgraph Z
+        subgraph X
+        a --> b
+        end
+        subgraph Y
+        c --> d
+        end
+        end
+        Y --> X
+        X --> P
+        P --> Y
+        `;
+
+    // Get layout data from flowDb
+    await flow.parse(flowchart);
+    const layoutData = flow.parser.yy.getData();
+
+    const svg: d3.Selection<SVGSVGElement, unknown, HTMLElement, any> = d3.select('svg');
+    const element = svg.select('g') as unknown as D3Selection<SVGElement>;
+    const graph = createGraphWithElements(element, layoutData);
+
+    // Call Layer Assignment for the graph
+    layerAssignment(layoutData);
+
+    // Call Node order Assignment for the graph
+    assignNodeOrder(50, layoutData);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'Z').order).toEqual(8);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'Y').order).toEqual(0.5);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'X').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'a').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'b').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'c').order).toEqual(0);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'd').order).toEqual(1);
+    expect(layoutData.nodes.find((node: Node) => node.id === 'P').order).toEqual(1);
+  });
+});
