387efb6a1d86b9dd56185fc1a9ebf731a64186eb
hkws <hkws0130@gmail.com>
2025-08-03 16:23:46 +0900

    add setupTooltip for state diagram

--- packages/mermaid/src/diagrams/state/stateRenderer-v3-unified.ts
+++ packages/mermaid/src/diagrams/state/stateRenderer-v3-unified.ts
@@ -84,9 +84,10 @@ export const draw = async function (text: string, id: string, _version: string, 
         return;
       }
 
-      const allNodes = svg.node()?.querySelectorAll('g');
+      const allNodes = svg.node()?.querySelectorAll('g.node');
       let matchedElem: SVGGElement | undefined;
 
+      // memo: ここでlabelとnodeどちらもmatchしてしまっていた
       allNodes?.forEach((g: SVGGElement) => {
         const text = g.textContent?.trim();
         if (text === stateId) {
@@ -112,6 +113,7 @@ export const draw = async function (text: string, id: string, _version: string, 
       if (linkInfo.tooltip) {
         const tooltip = linkInfo.tooltip.replace(/^"+|"+$/g, '');
         a.setAttribute('title', tooltip);
+        matchedElem.setAttribute('title', tooltip);
       }
 
       parent.replaceChild(a, matchedElem);
