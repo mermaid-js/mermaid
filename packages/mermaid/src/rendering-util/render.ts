a6d26ef6c3383f358f255d25e48f839afc112613
darshanr0107 <darshan@mermaidchart.com>
2025-08-12 18:38:22 +0530

    fix: remove cose-bilkent algorithm from tiny
    on-behalf-of: @Mermaid-Chart <hello@mermaidchart.com>

--- packages/mermaid/src/rendering-util/render.ts
+++ packages/mermaid/src/rendering-util/render.ts
@@ -39,10 +39,14 @@ const registerDefaultLayoutLoaders = () => {
       name: 'dagre',
       loader: async () => await import('./layout-algorithms/dagre/index.js'),
     },
-    {
-      name: 'cose-bilkent',
-      loader: async () => await import('./layout-algorithms/cose-bilkent/index.js'),
-    },
+    ...(includeLargeFeatures
+      ? [
+          {
+            name: 'cose-bilkent',
+            loader: async () => await import('./layout-algorithms/cose-bilkent/index.js'),
+          },
+        ]
+      : []),
   ]);
 };
 
