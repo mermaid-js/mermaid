24525815f7d5bdb45904de5132f680af9dd2dd2c
bwinsley <brandon.winsley@displayr.com>
2025-12-16 18:03:01 +1100

    Fix regex for comments and update tests

--- packages/mermaid/src/diagram-api/comments.spec.ts
+++ packages/mermaid/src/diagram-api/comments.spec.ts
@@ -58,7 +58,7 @@ graph TD
       "graph TD
 	A-->B
 	C-->D
-      "
+"
     `);
   });
 
@@ -88,102 +88,7 @@ graph TD
     expect(cleanupComments(text)).toMatchInlineSnapshot(`
       "graph TD
 	A-->B
-      "
-    `);
-  });
-});
-
-describe('block comments', () => {
-  it('should remove block comments on single lines', () => {
-    const text = `
-
-%%* This is a block comment *%%
-%%* This is another block comment *%%
-graph TD
-	A-->B
-%%* This is a block comment *%%
-`;
-    expect(cleanupComments(text)).toMatchInlineSnapshot(`
-      "graph TD
-      	A-->B
-      "
-    `);
-  });
-
-  it('should remove block comments on multiple lines', () => {
-    const text = `
-
-%%*
-This is a block comment
-*%%
-%%*
-This is another block comment
-*%%
-graph TD
-	A-->B
-%%*
-This is a block comment
-*%%
-`;
-    expect(cleanupComments(text)).toMatchInlineSnapshot(`
-      "graph TD
-      	A-->B
-      "
-    `);
-  });
-
-  it('should remove indented block comments', () => {
-    const text = `
-%%*
- This is a block comment
-        *%%
-        %%*
-This is a comment
-*%%
-graph TD
-	A-->B
-	      %%*
-        This is a block comment
-        *%%
-	C-->D
-`;
-    expect(cleanupComments(text)).toMatchInlineSnapshot(`
-      "graph TD
-	A-->B
-	C-->D
-      "
-    `);
-  });
-
-  it('should remove empty newlines from start', () => {
-    const text = `
-
-
-
-
-%%*
- This is a block comment
-*%%
-graph TD
-	A-->B
-`;
-    expect(cleanupComments(text)).toMatchInlineSnapshot(`
-      "graph TD
-      	A-->B
-      "
-    `);
-  });
-
-  it('should remove comments at end of text with no newline', () => {
-    const text = `
-graph TD
-	A-->B
-%%* This is a comment *%%`;
-
-    expect(cleanupComments(text)).toMatchInlineSnapshot(`
-      "graph TD
-	A-->B
-      "
+"
     `);
   });
 });
