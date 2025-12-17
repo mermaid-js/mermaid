1d2d744fc677eb0783918c2f138fc61564c8910c
darshanr0107 <darshan@mermaidchart.com>
2025-12-17 18:59:46 +0530

    fix: refactor createLabel
    on-behalf-of: @Mermaid-Chart <hello@mermaidchart.com>

--- packages/mermaid/src/rendering-util/rendering-elements/createLabel.js
+++ packages/mermaid/src/rendering-util/rendering-elements/createLabel.js
@@ -10,6 +10,12 @@ import { log } from '../../logger.js';
 import { decodeEntities } from '../../utils.js';
 
 /**
+ * Default width for wrapping text in labels.
+ * This matches the default value in the flowchart config schema.
+ */
+const DEFAULT_WRAPPING_WIDTH = 200;
+
+/**
  * @param dom
  * @param styleFn
  */
@@ -20,16 +26,25 @@ function applyStyle(dom, styleFn) {
 }
 
 /**
+ * Gets the wrapping width from config or returns the default.
+ * @returns {number} The wrapping width to use
+ */
+function getWrappingWidth() {
+  return getConfig().flowchart?.wrappingWidth ?? DEFAULT_WRAPPING_WIDTH;
+}
+
+/**
  * @param {any} node
  * @param {number} width
  * @param {boolean} addBackground
  * @returns {Promise<SVGForeignObjectElement>} Node
  */
-async function addHtmlLabel(node, width = 200, addBackground = false) {
+async function addHtmlLabel(node, width, addBackground = false) {
+  const labelWidth = width ?? getWrappingWidth();
   const fo = select(document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject'));
   // Set foreignObject dimensions first (same as createText)
-  fo.attr('width', `${10 * width}px`);
-  fo.attr('height', `${10 * width}px`);
+  fo.attr('width', `${10 * labelWidth}px`);
+  fo.attr('height', `${10 * labelWidth}px`);
 
   const div = fo.append('xhtml:div');
   const config = getConfig();
@@ -52,21 +67,31 @@ async function addHtmlLabel(node, width = 200, addBackground = false) {
   div.style('display', 'table-cell');
   div.style('white-space', 'nowrap');
   div.style('line-height', '1.5');
-  div.style('max-width', width + 'px');
+  div.style('max-width', labelWidth + 'px');
   div.style('text-align', 'center');
   div.attr('xmlns', 'http://www.w3.org/1999/xhtml');
   if (addBackground) {
     div.attr('class', 'labelBkg');
   }
 
+  const tempSvg = select(document.body)
+    .append('svg')
+    .attr('style', 'position: absolute; visibility: hidden; height: 0; width: 0;');
+  tempSvg.node().appendChild(fo.node());
+
   // Check if text needs wrapping (same logic as createText)
   let bbox = div.node().getBoundingClientRect();
-  if (bbox.width === width) {
+  if (bbox.width === labelWidth) {
     div.style('display', 'table');
     div.style('white-space', 'break-spaces');
-    div.style('width', width + 'px');
+    div.style('width', labelWidth + 'px');
     bbox = div.node().getBoundingClientRect();
   }
+
+  fo.attr('width', bbox.width);
+  fo.attr('height', bbox.height);
+
+  tempSvg.remove();
 
   return fo.node();
 }
@@ -104,9 +129,7 @@ const createLabel = async (
       ),
       labelStyle: style ? style.replace('fill:', 'color:') : style,
     };
-    // Use provided width or fall back to config
-    const labelWidth = width || getConfig().flowchart?.wrappingWidth || 200;
-    let vertexNode = await addHtmlLabel(node, labelWidth, addBackground);
+    let vertexNode = await addHtmlLabel(node, width, addBackground);
     // vertexNode.parentNode.removeChild(vertexNode);
     return vertexNode;
   } else {
