47d068b2aca81e920d566818d381fd77c286c2ae
darshanr0107 <darshan@mermaidchart.com>
2026-01-12 19:45:21 +0530

    chore:fix formatting issues on-behalf-of: @Mermaid-Chart <hello@mermaidchart.com>

--- packages/mermaid/src/dagre-wrapper/createLabel.js
+++ packages/mermaid/src/dagre-wrapper/createLabel.js
@@ -17,7 +17,6 @@ function applyStyle(dom, styleFn) {
     dom.attr('style', styleFn);
   }
 }
-
 /**
  * Gets the wrapping width from config or returns the default.
  * @returns {number} The wrapping width to use
@@ -25,7 +24,6 @@ function applyStyle(dom, styleFn) {
 function getWrappingWidth() {
   return getConfig().flowchart?.wrappingWidth ?? DEFAULT_WRAPPING_WIDTH;
 }
-
 /**
  * @param {any} node
  * @param {number | undefined} width
@@ -36,16 +34,13 @@ async function addHtmlLabel(node, width, addBackground = false) {
   const labelWidth = width ?? getWrappingWidth();
   const fo = select(document.createElementNS('http://www.w3.org/2000/svg', 'foreignObject'));
   fo.attr('width', 10000).attr('height', 10000);
-
   const div = fo.append('xhtml:div');
   const config = getConfig();
-
   const sanitizedLabel = hasKatex(node.label)
     ? await renderKatexSanitized(node.label.replace(common.lineBreakRegex, '\n'), config)
     : sanitizeText(node.label, config);
 
   const labelClass = node.isNode ? 'nodeLabel' : 'edgeLabel';
-
   const span = div.append('span');
   span.html(sanitizedLabel);
   applyStyle(span, node.labelStyle);
@@ -61,32 +56,24 @@ async function addHtmlLabel(node, width, addBackground = false) {
   if (addBackground) {
     div.attr('class', 'labelBkg');
   }
-
   const tempSvg = select(document.body)
     .append('svg')
     .attr('style', 'position: absolute; visibility: hidden; height: 0; width: 0;');
   tempSvg.node().appendChild(fo.node());
-
   // if there are images, need to wait for them to load before getting the bounding box
   await configureLabelImages(div.node(), node.label);
-
   // Check if text needs wrapping (same logic as createText)
   let bbox = div.node().getBoundingClientRect();
-
   if (bbox.width > labelWidth) {
     div
       .style('white-space', 'break-spaces')
       .style('max-width', labelWidth + 'px')
       .style('display', 'inline-block');
-
     bbox = div.node().getBoundingClientRect();
   }
-
   fo.attr('width', bbox.width);
   fo.attr('height', bbox.height);
-
   tempSvg.remove();
-
   return fo.node();
 }
 /**
