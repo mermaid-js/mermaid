490185e3a097e0269a0030489652ec3c845b07ce
darshanr0107 <darshan@mermaidchart.com>
2026-01-12 18:56:40 +0530

    fix: lint issues on-behalf-of: @Mermaid-Chart <hello@mermaidchart.com>

--- packages/mermaid/src/rendering-util/rendering-elements/edges.js
+++ packages/mermaid/src/rendering-util/rendering-elements/edges.js
@@ -51,6 +51,7 @@ export const getLabelStyles = (styleArray) => {
 
 export const insertEdgeLabel = async (elem, edge) => {
   let useHtmlLabels = getEffectiveHtmlLabels(getConfig());
+  const width = edge.width || getConfig().flowchart?.wrappingWidth;
 
   const { labelStyles } = styles2String(edge);
   edge.labelStyle = labelStyles;
