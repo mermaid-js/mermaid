c8f3fab7bcbf7ffae6661d3c84a7fde559e9aad8
omkarht <omkar@mermaidchart.com>
2026-01-30 16:19:35 +0530

    refactor: adjust padding and dimensions calculations for neo look in doubleCircle and rectLeftInvArrow shapes
    on-behalf-of: @Mermaid-Chart <hello@mermaidchart.com>

--- packages/mermaid/src/rendering-util/rendering-elements/shapes/doubleCircle.ts
+++ packages/mermaid/src/rendering-util/rendering-elements/shapes/doubleCircle.ts
@@ -12,19 +12,10 @@ export async function doublecircle<T extends SVGGraphicsElement>(
   node: Node
 ) {
   const { labelStyles, nodeStyles } = styles2String(node);
-  const gap = 12;
+  const gap = node.look === 'neo' ? 12 : 5;
   node.labelStyle = labelStyles;
-  // If incoming height & width are present, subtract the padding from them
-  // as labelHelper does not take padding into account
-  // also check if the width or height is less than minimum default values (50),
-  // if so set it to min value
   const padding = node.padding ?? 0;
   const labelPadding = node.look === 'neo' ? 16 : padding;
-
-  if (node.width || node.height) {
-    node.width = (node.width ?? 6) - labelPadding * 2;
-    node.height = node.width;
-  }
 
   const { shapeSvg, bbox } = await labelHelper(parent, node, getNodeClasses(node));
 
