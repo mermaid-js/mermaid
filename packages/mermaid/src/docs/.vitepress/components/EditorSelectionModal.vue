bbcbb66140a3953f5a72b679178e9c4fd2aeaa32
saurabhg772244 <saurabh@mermaidchart.com>
2026-02-02 13:43:14 +0530

    Update the CTA buttons in editor selection modal

--- packages/mermaid/src/docs/.vitepress/components/EditorSelectionModal.vue
+++ packages/mermaid/src/docs/.vitepress/components/EditorSelectionModal.vue
@@ -42,7 +42,7 @@ const editorColumns: EditorColumn[] = [
     redBarText: 'Recommended',
     proTrialButtonText: 'Start free trial',
     proTrialUrl:
-      'https://www.mermaidchart.com/app/sign-up?utm_source=mermaid_js&utm_medium=2_editor_selection&utm_campaign=start_plus&redirect=%2Fapp%2Fuser%2Fbilling%2Fcheckout%3FisFromMermaid%3Dtrue',
+      'https://www.mermaidchart.com/app/sign-up?utm_source=mermaid_js&utm_medium=2_editor_selection&utm_campaign=start_plus&redirect=%2Fapp%2Fuser%2Fbilling%2Fcheckout%3FisFromMermaid%3Dtrue%26tier%3Dplus',
     features: mermaidChartFeatures,
   },
   {
