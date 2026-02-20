a2a40163b44d855a5c8879d3aed63ddbb21d98ec
darshanr0107 <darshan@mermaidchart.com>
2026-02-20 16:23:08 +0530

    fix: apply drop shadow when using default theme
    on-behalf-of: @Mermaid-Chart <hello@mermaidchart.com>

--- packages/mermaid/src/themes/theme-default.js
+++ packages/mermaid/src/themes/theme-default.js
@@ -129,6 +129,12 @@ class Theme {
     this.labelColor = 'black';
     this.errorBkgColor = '#552222';
     this.errorTextColor = '#552222';
+
+    // Neo-specific
+    this.useGradient = false;
+    this.gradientStart = this.primaryBorderColor;
+    this.gradientStop = this.secondaryBorderColor;
+    this.dropShadow = 'drop-shadow(1px 2px 2px rgba(185, 185, 185, 1))';
     this.updateColors();
   }
   updateColors() {
