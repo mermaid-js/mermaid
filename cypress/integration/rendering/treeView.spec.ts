8949ef15f7ea8d6cc4cfd3ed70ae19e144563523
leentaylor <leentaylor@gmail.com>
2026-03-07 15:18:59 +1100

    update cypress

--- cypress/integration/rendering/treeView.spec.ts
+++ cypress/integration/rendering/treeView.spec.ts
@@ -4,42 +4,71 @@ describe('TreeView Diagram', () => {
   it('should render a simple treeView diagram', () => {
     imgSnapshotTest(
       `treeView-beta
-            file1.ts`
+            "file1.ts"`
     );
   });
 
   it('should render a complex treeView diagram', () => {
     imgSnapshotTest(
       `treeView-beta
-            root
-                folder1
-                    file1.js
-                    file2.ts
-                folder2
-                    file3.spec.ts
-                    folder3
-                        file4.ts
-                        file5.ts
-                        folder4
-                            file6.ts
-                file7.ts`
+            "root"
+                "folder1"
+                    "file1.js"
+                    "file2.ts"
+                "folder2"
+                    "file3.spec.ts"
+                    "folder3"
+                        "file4.ts"
+                        "file5.ts"
+                        "folder4"
+                            "file6.ts"
+                "file7.ts"`
     );
   });
 
   it('should render a complex treeView diagram with multiple roots', () => {
     imgSnapshotTest(
       `treeView-beta
-            folder1
-                file1.js
-                file2.ts
-            folder2
-                file3.spec.ts
-                folder3
-                    file4.ts
-                    file5.ts
-                    folder4
-                        file6.ts
-            file7.ts`
+            "folder1"
+                "file1.js"
+                "file2.ts"
+            "folder2"
+                "file3.spec.ts"
+                "folder3"
+                    "file4.ts"
+                    "file5.ts"
+                    "folder4"
+                        "file6.ts"
+            "file7.ts"`
+    );
+  });
+
+  it('should render a treeView diagram with custom config', () => {
+    imgSnapshotTest(
+      `
+      ---
+      config:
+        treeView:
+          rowIndent: 40
+          lineThickness: 2
+        themeVariables:
+          treeView:
+            fontSize: '30px'
+            textColor: '#FF0000'
+            lineColor: '#00FF00'
+      ---      
+      treeView-beta
+            "folder1"
+                "file1.js"
+                "file2.ts"
+            "folder2"
+                "file3.spec.ts"
+                "folder3"
+                    "file4.ts"
+                    "file5.ts"
+                    "folder4"
+                        "file6.ts"
+            "file7.ts"`
     );
   });
 });
