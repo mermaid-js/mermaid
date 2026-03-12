509cf15c6e9f40a0bc05d74eab19c00ba61bc95a
samkosal <67755908+samkosal@users.noreply.github.com>
2026-03-12 14:17:14 -0800

    fixed the issue of floating bars

--- packages/mermaid/src/diagrams/xychart/chartBuilder/components/plot/barPlot.ts
+++ packages/mermaid/src/diagrams/xychart/chartBuilder/components/plot/barPlot.ts
@@ -13,14 +13,11 @@ export class BarPlot {
   ) {}
 
   getDrawableElement(): DrawableElem[] {
+    const hasOffset = this.cumulativeOffsets.length > 0;
+
     const finalData: [number, number][] = this.barData.data.map((d, i) => {
       const offset = this.cumulativeOffsets[i] || 0;
       return [this.xAxis.getScaleValue(d[0]), this.yAxis.getScaleValue(d[1] + offset)];
-    });
-
-    const baselineData: number[] = this.barData.data.map((_, i) => {
-      const offset = this.cumulativeOffsets[i] || 0;
-      return this.yAxis.getScaleValue(offset);
     });
 
     const barPaddingPercent = 0.05;
@@ -31,23 +28,24 @@ export class BarPlot {
     const barWidthHalf = barWidth / 2;
 
     if (this.orientation === 'horizontal') {
-      const baselineX: number[] = this.barData.data.map((_, i) => {
-        const offset = this.cumulativeOffsets[i] || 0;
-        return this.xAxis.getScaleValue(offset);
-      });
       return [
         {
           groupTexts: ['plot', `bar-plot-${this.plotIndex}`],
           type: 'rect',
-          data: finalData.map((data, i) => ({
-            x: baselineX[i],
-            y: data[0] - barWidthHalf,
-            height: barWidth,
-            width: data[1] - baselineX[i],
-            fill: this.barData.fill,
-            strokeWidth: 0,
-            strokeFill: this.barData.fill,
-          })),
+          data: finalData.map((data, i) => {
+            const baseX = hasOffset
+              ? this.yAxis.getScaleValue(this.cumulativeOffsets[i] || 0)
+              : this.boundingRect.x;
+            return {
+              x: baseX,
+              y: data[0] - barWidthHalf,
+              height: barWidth,
+              width: data[1] - baseX,
+              fill: this.barData.fill,
+              strokeWidth: 0,
+              strokeFill: this.barData.fill,
+            };
+          }),
         },
       ];
     }
@@ -55,15 +53,20 @@ export class BarPlot {
       {
         groupTexts: ['plot', `bar-plot-${this.plotIndex}`],
         type: 'rect',
-        data: finalData.map((data, i) => ({
-          x: data[0] - barWidthHalf,
-          y: data[1],
-          width: barWidth,
-          height: baselineData[i] - data[1],
-          fill: this.barData.fill,
-          strokeWidth: 0,
-          strokeFill: this.barData.fill,
-        })),
+        data: finalData.map((data, i) => {
+          const baseY = hasOffset
+            ? this.yAxis.getScaleValue(this.cumulativeOffsets[i] || 0)
+            : this.boundingRect.y + this.boundingRect.height;
+          return {
+            x: data[0] - barWidthHalf,
+            y: data[1],
+            width: barWidth,
+            height: baseY - data[1],
+            fill: this.barData.fill,
+            strokeWidth: 0,
+            strokeFill: this.barData.fill,
+          };
+        }),
       },
     ];
   }
