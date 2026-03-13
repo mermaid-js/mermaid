bf5955ca11f5e6d8435250a01a661713445cbb78
darshanr0107 <darshan@mermaidchart.com>
2026-03-13 15:12:13 +0530

    chore:add a test for requirement diagram in classic look with default theme
    on-behalf-of: @Mermaid-Chart <hello@mermaidchart.com>

--- cypress/integration/rendering/requirementDiagram-neo-themes.spec.ts
+++ cypress/integration/rendering/requirementDiagram-neo-themes.spec.ts
@@ -387,3 +387,24 @@ title: Requirement Diagram Title
     });
   });
 });
+
+describe('Requirement diagrams in classic look (regression)', () => {
+  it('should render a basic requirement diagram with relationship in classic look and default theme', () => {
+    imgSnapshotTest(
+      `
+      requirementDiagram
+          requirement test_req {
+              id: 1
+              text: the test text.
+              risk: high
+              verifymethod: test
+          }
+          element test_entity {
+              type: simulation
+          }
+          test_entity - satisfies -> test_req
+      `,
+      { look: 'classic', theme: 'default' }
+    );
+  });
+});
