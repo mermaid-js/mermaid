64cfeb4ae5973d05889aebb468d0b38d351c2707
darshanr0107 <darshan@mermaidchart.com>
2026-03-16 17:11:55 +0530

    fix: streamline state diagram tests on-behalf-of: @Mermaid-Chart <hello@mermaidchart.com>

--- cypress/integration/rendering/stateDiagram-neo.spec.js
+++ cypress/integration/rendering/stateDiagram-neo.spec.js
@@ -1,6 +1,6 @@
 import { imgSnapshotTest } from '../../helpers/util.ts';
 
-const looks = ['neo', 'classic'];
+const looks = ['neo'];
 const themes = ['neo', 'neo-dark', 'redux', 'redux-dark', 'redux-color', 'redux-dark-color'];
 
 // State diagram feature sets to test
@@ -65,10 +65,22 @@ const stateFeatureSets = [
   },
 ];
 
+describe('State diagram classic look and default theme', () => {
+  it('should render a simple state diagram', () => {
+    imgSnapshotTest(
+      `stateDiagram-v2
+    [*] --> State1
+    State1 --> State2
+    State2 --> [*]`,
+      { look: 'classic', theme: 'default' }
+    );
+  });
+});
+
 looks.forEach((look) => {
   themes.forEach((theme) => {
     stateFeatureSets.forEach((featureSet, setIndex) => {
-      describe(`Test ${featureSet.name} in ${look} look and ${theme} theme - set ${setIndex + 1}`, () => {
+      describe(`Test ${featureSet.name} in ${look} look and ${theme} theme`, () => {
         it(`should render ${featureSet.name}`, () => {
           let stateDiagramCode;
           if (featureSet.states) {
@@ -95,53 +107,11 @@ looks.forEach((look) => {
     });
 
     describe(`State diagram ${look} look and ${theme} theme - Additional Tests`, () => {
-      it('should render a simple info', () => {
-        imgSnapshotTest(`info`, { logLevel: 1, fontFamily: 'courier', look, theme });
-      });
-
-      it('should render a simple state diagrams', () => {
+      it('should render a simple state diagram', () => {
         imgSnapshotTest(
           `stateDiagram
     [*] --> State1
     State1 --> [*]`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a long descriptions instead of id when available', () => {
-        imgSnapshotTest(
-          `stateDiagram
-      [*] --> S1
-      state "Some long name" as S1`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a long descriptions with additional descriptions', () => {
-        imgSnapshotTest(
-          `stateDiagram
-      [*] --> S1
-      state "Some long name" as S1: The description`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a single state with short descriptions', () => {
-        imgSnapshotTest(
-          `stateDiagram
-      state "A long long name" as long1
-      state "A" as longlonglongid`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a transition descriptions with new lines', () => {
-        imgSnapshotTest(
-          `stateDiagram
-      [*] --> S1
-      S1 --> S2: long line using<br/>should work
-      S1 --> S3: long line using <br>should work
-      S1 --> S4: long line using \\nshould work`,
           { logLevel: 0, fontFamily: 'courier', look, theme }
         );
       });
@@ -158,82 +128,7 @@ looks.forEach((look) => {
         );
       });
 
-      it('should render a state with on the left side when so specified', () => {
-        imgSnapshotTest(
-          `stateDiagram
-    State1: The state with a note with minus - and plus + in it
-    note left of State1
-      Important information! You can write
-      notes with . and  in them.
-    end note`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a state with a note together with another state', () => {
-        imgSnapshotTest(
-          `stateDiagram
-    State1: The state with a note +,-
-    note right of State1
-      Important information! You can write +,-
-      notes.
-    end note
-    State1 --> State2 : With +,-
-    note left of State2 : This is the note +,-<br/>`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a note with multiple lines in it', () => {
-        imgSnapshotTest(
-          `stateDiagram
-    State1: The state with a note
-    note right of State1
-      Important information! You\\ncan write
-      notes with multiple lines...
-      Here is another line...
-      And another line...
-    end note`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should handle multiline notes with different line breaks', () => {
-        imgSnapshotTest(
-          `stateDiagram
-      State1
-      note right of State1
-      Line1<br>Line2<br/>Line3<br />Line4<br/>Line5
-      end note`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a states with descriptions including multi-line descriptions', () => {
-        imgSnapshotTest(
-          `stateDiagram
-    State1: This a single line description
-    State2: This a multi line description
-    State2: here comes the multi part
-    [*] --> State1
-    State1 --> State2
-    State2 --> [*]`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a simple state diagrams 2', () => {
-        imgSnapshotTest(
-          `stateDiagram
-    [*] --> State1
-    State1 --> State2
-    State1 --> State3
-    State1 --> [*]`,
-          { logLevel: 0, fontFamily: 'courier', look, theme }
-        );
-      });
-
-      it('should render a simple state diagrams with labels', () => {
+      it('should render transitions with labels', () => {
         imgSnapshotTest(
           `stateDiagram
     [*] --> State1
