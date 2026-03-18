ef671bf00335151f1b8139e1d43f751a53e1194a
Nabila Ben Ammar <nabilabenammar0@gmail.com>
2026-03-18 17:55:23 +0100

    fix: ignore root CHANGELOG.md symlink in ESLint config to prevent CI file-not-found error

--- eslint.config.js
+++ eslint.config.js
@@ -25,6 +25,7 @@ export default tseslint.config(
       '**/coverage/',
       'packages/mermaid/src/config.type.ts',
       'packages/mermaid/src/docs/.vitepress/components.d.ts',
+      'CHANGELOG.md',
     ],
   },
   {
