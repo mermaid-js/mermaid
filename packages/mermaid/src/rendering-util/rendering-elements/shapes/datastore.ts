15b16ec068f0b6396e20570aefa803453d75f603
Arun Chandanaveli <aruncveli@gmail.com>
2026-03-28 09:52:17 +0000

    fix handDrawn look

--- packages/mermaid/src/rendering-util/rendering-elements/shapes/datastore.ts
+++ packages/mermaid/src/rendering-util/rendering-elements/shapes/datastore.ts
@@ -1,6 +1,8 @@
 import type { Node, RectOptions } from '../../types.js';
 import type { D3Selection } from '../../../types.js';
 import { drawRect } from './drawRect.js';
+import { userNodeOverrides } from './handDrawnShapeStyles.js';
+import rough from 'roughjs';
 
 export async function datastore<T extends SVGGraphicsElement>(parent: D3Selection<T>, node: Node) {
   const { cssClasses, labelPaddingX, labelPaddingY, padding, width, height } = node;
@@ -14,15 +16,33 @@ export async function datastore<T extends SVGGraphicsElement>(parent: D3Selectio
   };
 
   const rect = await drawRect(parent, node, options);
-  const datastoreShape = rect.select('.basic.label-container');
-  const datastoreWidth = (Number(datastoreShape.attr('width')) || width) ?? 0;
-  const datastoreHeight = (Number(datastoreShape.attr('height')) || height) ?? 0;
 
-  if (datastoreWidth > 0 && datastoreHeight > 0) {
-    datastoreShape.attr(
-      'stroke-dasharray',
-      [datastoreWidth, 0, 0, datastoreHeight, datastoreWidth, 0, 0, datastoreHeight].join(' ')
+  if (node.look === 'handDrawn') {
+    // @ts-ignore TODO: Fix rough typings
+    const rc = rough.svg(rect);
+    const options = userNodeOverrides(node, {});
+
+    const borderSelection = rect.select('.basic.label-container > path:nth-child(2)');
+    const borderPath = borderSelection.node() as SVGPathElement;
+    const bb = borderPath.getBBox();
+
+    rect.insert(
+      () => rc.line(bb.x, bb.y, bb.x + bb.width, bb.y, options),
+      '.basic.label-container g.label'
     );
+    rect.insert(
+      () => rc.line(bb.x, bb.y + bb.height, bb.x + bb.width, bb.y + bb.height, options),
+      '.basic.label-container g.label'
+    );
+    borderSelection.remove();
+    return rect;
+  }
+
+  const selection = rect.select('.basic.label-container');
+  const datastoreWidth = (Number(selection.attr('width')) || width) ?? 0;
+  const datastoreHeight = (Number(selection.attr('height')) || height) ?? 0;
+  if (datastoreWidth > 0 && datastoreHeight > 0) {
+    selection.attr('stroke-dasharray', `${datastoreWidth} ${datastoreHeight}`);
   }
 
   return rect;
