2b35f49cf31f731c82441fcc2c7248491b45d454
Per Brolin <per@mermaidchart.com>
2026-04-17 15:16:00 +0200

    Test feature

--- packages/mermaid/src/diagrams/architecture/architecture.spec.ts
+++ packages/mermaid/src/diagrams/architecture/architecture.spec.ts
@@ -10,6 +10,7 @@ describe('architecture diagrams', () => {
     parser.parser?.yy = db;
   });
 
+  // Added comment just to trigg test. REMOVE !!!
   describe('architecture diagram definitions', () => {
     it('should handle the architecture keyword', async () => {
       const str = `architecture-beta`;
