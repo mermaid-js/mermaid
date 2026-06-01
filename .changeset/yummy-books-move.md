96c73811d8d27a82773e4d1c96ca03e55d8c56cb
ngdaniels <ngdanielsumardi@gmail.com>
2026-06-01 22:35:47 +0700

    add test and changeset

---
+++ .changeset/yummy-books-move.md
@@ -0,0 +1,5 @@
+---
+'mermaid': minor
+---
+
+Bug: XY chart - fix chart when x label is written after data
--- cypress/integration/rendering/xychart/xyChart.spec.js
+++ cypress/integration/rendering/xychart/xyChart.spec.js
@@ -1007,4 +1007,17 @@ describe('XY Chart', () => {
       {}
     );
   });
+
+  it('should render correctly when X label is written after data', () => {
+    imgSnapshotTest(
+      `
+      xychart
+        title Price
+        bar [5000, 6000, 7500, 8200, 9500, 10500, 11000, 10200, 9200, 8500, 7000, 6000]
+        line [5000, 6000, 7500, 8200, 9500, 10500, 11000, 10200, 9200, 8500, 7000, 6000]
+        x-axis [jan, feb, mar, apr, may, jun, jul, aug, sep, oct, nov, dec]
+      `,
+      {}
+    );
+  });
 });
