mermaid-js/mermaid

commit 64769738d5b59211e1decb471ffbaca8afec51aa

fix: prevent CSS namespace escape using `:not(&)`
Currently, we're namespacing user's custom CSS to only apply within
the `<div id='svgId'>` by doing:

```css
 #svgId {
  .myCssClass { /* my rules here */ }
}
```

If there's no `&` in the child rule selector, one gets prefixed
[automatically][1], which namespaces the rule correctly.

```css
 #svgId {
  & .myCssClass { /* my rules here */}
}
```

However, if an `&` is present in the child rule selector, there's no
automatic prefix of the parent rule selector, which allows user defined
CSS to escape the `<div id='svgId'>`, e.g.

```css
 #svgId {
  :not(&) { /* my rules here */ }
}
```

This commit adds a stylis middleware that automatically prefixes
`#svgId` to any rule selector if it's not already there, preventing this
bypass.

[1]: https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Selectors/Nesting_selector

packages/mermaid/src/mermaidAPI.ts
@@ -4,7 +4,7 @@
  */
 // @ts-ignore TODO: Investigate D3 issue
 import { select } from 'd3';
-import { compile, serialize, stringify } from 'stylis';
+import { compile, middleware, serialize, stringify } from 'stylis';
 import DOMPurify from 'dompurify';
 import isEmpty from 'lodash-es/isEmpty.js';
 import { addSVGa11yTitleDescription, setA11yDiagramInfo } from './accessibility.js';
@@ -188,6 +188,50 @@ export const createCssStyles = (
   return cssString + cssStyleSheetToString(cssStyles);
 };
 
+/**
+ * Use `stylis` to compile the CSS to only apply to the given namespace.
+ *
+ * This will also remove some newer CSS features (e.g. nesting) to better
+ * support older browsers and does some minification.
+ *
+ * @internal
+ * @param namespace - the namespace to add in front of all the CSS styles, e.g. `#idOfSvgElement`
+ * @param css - the CSS styles to add the namespace to.
+ * @see https://github.com/thysultan/stylis
+ *
+ * @example
+ * // Returns `#id .class1{fill:red;}`
+ * compileCSS('#id', `.class1 { fill: red }`)
+ */
+const compileCSS = (namespace: `#${string}`, css: string) => {
+  return serialize(
+    compile(`${namespace}{${css}}`),
+    middleware([
+      function addNamespace(element, _index, _children, _callback) {
+        /**
+         * CSS normally automatically adds the `&` selector in front of each
+         * element. But, if there's already an `&` selector, it doesn't add this.
+         *
+         * This code will explicitly make sure it's always added, to ensure
+         * that the CSS never applies outside the SVG.
+         *
+         * E.g. `#svgId { .nested-class :not(&) { fill: red } }` will be
+         * transformed to `#svgId { & .nested-class :not(&) { fill: red } }`
+         */
+        if (element.type === 'rule' && Array.isArray(element.props)) {
+          element.props = element.props.map((prop) => {
+            if (!prop.startsWith(namespace)) {
+              return `${namespace} ${prop}`;
+            }
+            return prop;
+          });
+        }
+      },
+      stringify,
+    ])
+  );
+};
+
 export const createUserStyles = (
   config: MermaidConfig,
   graphType: string,
@@ -202,11 +246,7 @@ export const createUserStyles = (
     { ...config.themeVariables, theme: config.theme, look: config.look },
     svgId
   );
-
-  // Now turn all of the styles into a (compiled) string that starts with the id
-  // use the stylis library to compile the css, turn the results into a valid CSS string (serialize(...., stringify))
-  // @see https://github.com/thysultan/stylis
-  return serialize(compile(`${svgId}{${allStyles}}`), stringify);
+  return compileCSS(svgId, allStyles);
 };
 
 /**

packages/mermaid/src/mermaidAPI.spec.ts
@@ -461,6 +461,21 @@ describe('mermaidAPI', () => {
         '#someId .edge-pattern-dashed{stroke-dasharray:3;}#someId .default{color:red;}#someId .classDef2>*{color:purple;}#someId .classDef2 span{color:purple;}'
       );
     });
+
+    it('should handle `:not(&)` selectors in the CSS', () => {
+      const result = createUserStyles(
+        {
+          ...mockConfig,
+          themeCSS: ':not(&){background:green !important}',
+        },
+        'someDiagram',
+        new Map(),
+        '#someId'
+      );
+      expect(result).toEqual(
+        '#someId .edge-pattern-dashed{stroke-dasharray:3;}#someId :not(#someId){background:green!important;}'
+      );
+    });
   });
 
   describe('removeExistingElements', () => {

cypress/integration/other/ghsa.spec.js
@@ -41,4 +41,18 @@ describe('CSS injections', () => {
     );
     cy.get('body > div #pwned').should('not.exist');
   });
+  it('should prevent CSS namespace injection via :not(&)', () => {
+    imgSnapshotTest(
+      `---
+title: Green background CSS should not be able to escape the diagram using :not(&)
+config:
+  themeCSS: ':not(&){background:green !important}'
+---
+flowchart
+  A --> B
+     `,
+      { logLevel: 1 }
+    );
+    cy.get('body').should('not.have.css', 'background-color', 'rgb(0, 128, 0)');
+  });
 });

commit 12d472c9ed43f94814b110da8d7a9ae6dd5266ed

Merge commit from fork
Stylis compiles `& ~ *` and `& + *` to `#svgId ~ *` / `#svgId + *`,
which select DOM siblings of the SVG element (aka outside of the SVG
boundary).

This commit changes the `addNamespace` middleware to detect when a
selector uses one of these, and namespaces it properly, e.g.
`#svgId ~ *` into `#svgId #svgId ~ *`.

I've also added support for handling the [column combinator `||`][1].
It's experimental, but there's no reason not to handle it.

For `& {`, I had to do some special handling. Technically, it's not
namespaced, but namespacing it with `& &` breaks, and replacing it with
`& > *` overwrites some SVG presentation attributes, so it breaks
existing diagrams. Instead, I've just added a quick check that allows it
if-and-only-if it only has `font-family`, `font-size`, or `fill` rules.

[1]: https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Selectors/Column_combinator

Reported-by: h3ri0s <rohitprasanth8@gmail.com>
See: https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Selectors/Next-sibling_combinator
See: https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Selectors/Subsequent-sibling_combinator
Fixes: 64769738d5b59211e1decb471ffbaca8afec51aa

packages/mermaid/src/mermaidAPI.ts
@@ -238,7 +238,37 @@ const compileCSS = (namespace: `#${string}`, css: string) => {
             return;
           }
           element.props = element.props.map((prop) => {
-            if (!prop.startsWith(namespace)) {
+            /**
+             * For the root selector `& { ... }`, allow limited inherited
+             * styles to not be namespaced.
+             * These won't do anything to the `<svg>`, but will be inherited by
+             * children with a lower specificity than SVG presentation attributes.
+             */
+            if (
+              prop === namespace &&
+              Array.isArray(element.children) &&
+              element.children.every((child) => {
+                if (child.type !== 'decl') {
+                  return false;
+                }
+                const allowedProps = new Set<typeof child.props>([
+                  'font-family',
+                  'font-size',
+                  'fill',
+                ]);
+                return allowedProps.has(child.props);
+              })
+            ) {
+              return prop;
+            }
+
+            const alreadyNamespaced =
+              // If the prop already starts with the namespace followed by a space or >, then it's already namespaced.
+              (prop.startsWith(`${namespace} `) || prop.startsWith(`${namespace}>`)) &&
+              // Column combinators are not yet widely supported, it's not yet compressed to `${namespace}||`,
+              // so we need to add an extra check for that
+              !prop.startsWith(`${namespace} ||`);
+            if (!alreadyNamespaced) {
               return `${namespace} ${prop}`;
             }
             return prop;

packages/mermaid/src/styles.ts
@@ -39,7 +39,7 @@ const getStyles = (
   } else {
     log.warn(`No theme found for ${type}`);
   }
-  return ` & {
+  return `& {
     font-family: ${options.fontFamily};
     font-size: ${options.fontSize};
     fill: ${options.textColor}

packages/mermaid/src/mermaidAPI.spec.ts
@@ -477,6 +477,34 @@ describe('mermaidAPI', () => {
       );
     });
 
+    it.for([
+      // test for CSS sibling components
+      ['should namespace sibling combinator', '& ~ *', '#someId #someId~*'],
+      // CSS ignores these whitespace characters
+      ['should namespace sibling combinator', '& \n\t \r \f \r\n + *', '#someId #someId+*'],
+      /*
+       * Experimental column combinator,
+       * see https://developer.mozilla.org/en-US/docs/Web/CSS/Reference/Selectors/Column_combinator
+       *
+       * Output whitespace will probably change once JSDOM supports it.
+       */
+      ['should namespace column combinator', '& || *', '#someId #someId || *'],
+      ['should namespace root if using unsupported declarations', '&', '#someId #someId'],
+      // child combinators and descent combinators are already namespaced
+      ['should not namespace child combinator', '& > *', '#someId>*'],
+      ['should not namespace descent combinator', '& *', '#someId *'],
+    ] as const)('%s %s to %s', ([_description, inputSelector, expectedSelector]) => {
+      const result = createUserStyles(
+        { ...mockConfig, themeCSS: `${inputSelector} { color: red; }` },
+        'someDiagram',
+        new Map(),
+        '#someId'
+      );
+      expect(result).toEqual(
+        `#someId .edge-pattern-dashed{stroke-dasharray:3;}${expectedSelector}{color:red;}`
+      );
+    });
+
     it('should remove unsupported at-rules from user CSS', () => {
       const result = createUserStyles(
         {